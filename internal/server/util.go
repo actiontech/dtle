e145354a0ca1f912746486ce657f03982e35f78d
fudongbiao <fudongbiao@actionsky.com>
2017-04-14 17:00:27 +0800

    add/remove raft peers

--- internal/server/util.go
+++ internal/server/util.go
@@ -1,8 +1,14 @@
 package server
 
 import (
+	"fmt"
+	"net"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strconv"
+
+	"github.com/hashicorp/serf/serf"
 )
 
 // ensurePath is used to make sure a path exists
@@ -11,4 +17,80 @@ func ensurePath(path string, dir bool) error {
 		path = filepath.Dir(path)
 	}
 	return os.MkdirAll(path, 0755)
+}
+
+// runtimeStats is used to return various runtime information
+func runtimeStats() map[string]string {
+	return map[string]string{
+		"os":         runtime.GOOS,
+		"arch":       runtime.GOARCH,
+		"version":    runtime.Version(),
+		"max_procs":  strconv.FormatInt(int64(runtime.GOMAXPROCS(0)), 10),
+		"goroutines": strconv.FormatInt(int64(runtime.NumGoroutine()), 10),
+		"cpu_count":  strconv.FormatInt(int64(runtime.NumCPU()), 10),
+	}
+}
+
+// serverParts is used to return the parts of a server role
+type serverParts struct {
+	Name       string
+	Region     string
+	Datacenter string
+	Port       int
+	Bootstrap  bool
+	Expect     int
+	Version    int
+	Addr       net.Addr
+}
+
+func (s *serverParts) String() string {
+	return fmt.Sprintf("%s (Addr: %s) (DC: %s)",
+		s.Name, s.Addr, s.Datacenter)
+}
+
+// Returns if a member is a Udup server. Returns a boolean,
+// and a struct with the various important components
+func isUdupServer(m serf.Member) (bool, *serverParts) {
+	if m.Tags["role"] != "udup" {
+		return false, nil
+	}
+
+	region := m.Tags["region"]
+	datacenter := m.Tags["dc"]
+	_, bootstrap := m.Tags["bootstrap"]
+
+	expect := 0
+	expect_str, ok := m.Tags["expect"]
+	var err error
+	if ok {
+		expect, err = strconv.Atoi(expect_str)
+		if err != nil {
+			return false, nil
+		}
+	}
+
+	port_str := m.Tags["port"]
+	port, err := strconv.Atoi(port_str)
+	if err != nil {
+		return false, nil
+	}
+
+	vsn_str := m.Tags["vsn"]
+	vsn, err := strconv.Atoi(vsn_str)
+	if err != nil {
+		return false, nil
+	}
+
+	addr := &net.TCPAddr{IP: m.Addr, Port: port}
+	parts := &serverParts{
+		Name:       m.Name,
+		Region:     region,
+		Datacenter: datacenter,
+		Port:       port,
+		Bootstrap:  bootstrap,
+		Expect:     expect,
+		Addr:       addr,
+		Version:    vsn,
+	}
+	return true, parts
 }
