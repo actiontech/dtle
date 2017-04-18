e4332e5d2412f753f8bfa3dc90405cb5ea232304
fudongbiao <fudongbiao@actionsky.com>
2017-04-18 13:45:06 +0800

    add plan queue

---
+++ internal/models/plan.go
@@ -0,0 +1,152 @@
+package models
+
+// Plan is used to submit a commit plan for task allocations. These
+// are submitted to the leader which verifies that resources have
+// not been overcommitted before admiting the plan.
+type Plan struct {
+	// EvalID is the evaluation ID this plan is associated with
+	EvalID string
+
+	// EvalToken is used to prevent a split-brain processing of
+	// an evaluation. There should only be a single scheduler running
+	// an Eval at a time, but this could be violated after a leadership
+	// transition. This unique token is used to reject plans that are
+	// being submitted from a different leader.
+	EvalToken string
+
+	// Job is the parent job of all the allocations in the Plan.
+	// Since a Plan only involves a single Job, we can reduce the size
+	// of the plan by only including it once.
+	Job *Job
+
+	// NodeUpdate contains all the allocations for each node. For each node,
+	// this is a list of the allocations to update to either stop or evict.
+	NodeUpdate map[string][]*Allocation
+
+	// NodeAllocation contains all the allocations for each node.
+	// The evicts must be considered prior to the allocations.
+	NodeAllocation map[string][]*Allocation
+
+	// Annotations contains annotations by the scheduler to be used by operators
+	// to understand the decisions made by the scheduler.
+	Annotations *PlanAnnotations
+}
+
+// AppendUpdate marks the allocation for eviction. The clientStatus of the
+// allocation may be optionally set by passing in a non-empty value.
+func (p *Plan) AppendUpdate(alloc *Allocation, desiredStatus, desiredDesc, clientStatus string) {
+	newAlloc := new(Allocation)
+	*newAlloc = *alloc
+
+	// If the job is not set in the plan we are deregistering a job so we
+	// extract the job from the allocation.
+	if p.Job == nil && newAlloc.Job != nil {
+		p.Job = newAlloc.Job
+	}
+
+	// Normalize the job
+	newAlloc.Job = nil
+
+	newAlloc.DesiredStatus = desiredStatus
+	newAlloc.DesiredDescription = desiredDesc
+
+	if clientStatus != "" {
+		newAlloc.ClientStatus = clientStatus
+	}
+
+	node := alloc.NodeID
+	existing := p.NodeUpdate[node]
+	p.NodeUpdate[node] = append(existing, newAlloc)
+}
+
+func (p *Plan) PopUpdate(alloc *Allocation) {
+	existing := p.NodeUpdate[alloc.NodeID]
+	n := len(existing)
+	if n > 0 && existing[n-1].ID == alloc.ID {
+		existing = existing[:n-1]
+		if len(existing) > 0 {
+			p.NodeUpdate[alloc.NodeID] = existing
+		} else {
+			delete(p.NodeUpdate, alloc.NodeID)
+		}
+	}
+}
+
+func (p *Plan) AppendAlloc(alloc *Allocation) {
+	node := alloc.NodeID
+	existing := p.NodeAllocation[node]
+	p.NodeAllocation[node] = append(existing, alloc)
+}
+
+// IsNoOp checks if this plan would do nothing
+func (p *Plan) IsNoOp() bool {
+	return len(p.NodeUpdate) == 0 && len(p.NodeAllocation) == 0
+}
+
+// PlanResult is the result of a plan submitted to the leader.
+type PlanResult struct {
+	// NodeUpdate contains all the updates that were committed.
+	NodeUpdate map[string][]*Allocation
+
+	// NodeAllocation contains all the allocations that were committed.
+	NodeAllocation map[string][]*Allocation
+
+	// RefreshIndex is the index the worker should refresh state up to.
+	// This allows all evictions and allocations to be materialized.
+	// If any allocations were rejected due to stale data (node state,
+	// over committed) this can be used to force a worker refresh.
+	RefreshIndex uint64
+
+	// AllocIndex is the Raft index in which the evictions and
+	// allocations took place. This is used for the write index.
+	AllocIndex uint64
+}
+
+// IsNoOp checks if this plan result would do nothing
+func (p *PlanResult) IsNoOp() bool {
+	return len(p.NodeUpdate) == 0 && len(p.NodeAllocation) == 0
+}
+
+// FullCommit is used to check if all the allocations in a plan
+// were committed as part of the result. Returns if there was
+// a match, and the number of expected and actual allocations.
+func (p *PlanResult) FullCommit(plan *Plan) (bool, int, int) {
+	expected := 0
+	actual := 0
+	for name, allocList := range plan.NodeAllocation {
+		didAlloc, _ := p.NodeAllocation[name]
+		expected += len(allocList)
+		actual += len(didAlloc)
+	}
+	return actual == expected, expected, actual
+}
+
+// PlanAnnotations holds annotations made by the scheduler to give further debug
+// information to operators.
+type PlanAnnotations struct {
+	// DesiredTGUpdates is the set of desired updates per task.
+	DesiredTGUpdates map[string]*DesiredUpdates
+}
+
+// PlanRequest is used to submit an allocation plan to the leader
+type PlanRequest struct {
+	Plan *Plan
+	WriteRequest
+}
+
+// PlanResponse is used to return from a PlanRequest
+type PlanResponse struct {
+	Result *PlanResult
+	WriteMeta
+}
+
+// DesiredUpdates is the set of changes the scheduler would like to make given
+// sufficient resources and cluster capacity.
+type DesiredUpdates struct {
+	Ignore            uint64
+	Place             uint64
+	Migrate           uint64
+	Stop              uint64
+	InPlaceUpdate     uint64
+	DestructiveUpdate uint64
+}
