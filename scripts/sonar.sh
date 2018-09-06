5b35ff75e200a7d9725a9221eab87acb569f3798
ffffwh <ffffwh@icloud.com>
2018-09-06 11:23:37 +0800

    add sonar scan script
    and update gitignore

---
+++ scripts/sonar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f Gopkg.toml ]; then
+    echo "This script must be run under project root."
+    exit 1
+fi
+
+sonar-scanner \
+  -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+  -Dsonar.sources="$PWD" \
+  -Dsonar.host.url="$SONAR_HOST" \
+  -Dsonar.login="$SONAR_TOKEN" \
+  '-Dsonar.exclusions=vendor/**,dist/**'
