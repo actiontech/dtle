3e893ffa44c183f74618dfc9c51ddfbeb8f9ac3c
ffffwh <ffffwh@icloud.com>
2018-10-16 16:17:33 +0800

    unit test for 'where'

--- internal/config/where_test.go
+++ internal/config/where_test.go
@@ -5,27 +5,72 @@ import (
 	"testing"
 )
 
-func TestWhereTrue(t *testing.T) {
-	var err error
-	table := NewTable("a", "a")
-	table.OriginalTableColumns = mysql.NewColumnList(mysql.NewColumns([]string{
-		"id", "a",
-	}))
-	whereCtx, err := NewWhereCtx("a % 2 = 0", table)
+func newTableContextWithWhere(t *testing.T,
+	schemaName string, tableName string, where string, columnNames ...string) *TableContext {
+
+	table := NewTable(schemaName, tableName)
+	table.OriginalTableColumns = mysql.NewColumnList(mysql.NewColumns(columnNames))
+	whereCtx, err := NewWhereCtx(where, table)
 	if err != nil {
-		t.Fail()
+		t.Fatal(err)
 	}
 	tbCtx := NewTableContext(table, whereCtx)
+	return tbCtx
+}
+func buildColumnValues(vals ...interface{}) *mysql.ColumnValues {
+	return mysql.ToColumnValues(vals)
+}
 
-	for i := 0; i < 10000; i++ {
-		absValues := []interface{}{i, i}
-		colValues := mysql.ToColumnValues(absValues)
-		b, err := tbCtx.WhereTrue(colValues)
+func TestWhereTrue(t *testing.T) {
+	var tbCtx *TableContext
+
+	tbCtx = newTableContextWithWhere(t, "db1", "tb1", "a % 2 = 0", "id", "a")
+	for i := 0; i < 1000; i++ {
+		r, err := tbCtx.WhereTrue(buildColumnValues(i, i))
 		if err != nil {
-			t.Fail()
+			t.Fatal(err)
 		}
-		if b != (i % 2 == 0) {
-			t.Errorf("i: %v, b: %v", i, b)
+		if r != (i % 2 == 0) {
+			t.Fatalf("i: %v, r: %v", i, r)
+		}
+	}
+
+	////////
+
+	tbCtx = newTableContextWithWhere(t, "db1", "tb1", "a < 5", "id", "a")
+	for i := -100; i < 1000; i++ {
+		r, err := tbCtx.WhereTrue(buildColumnValues(i, i))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != (i < 5) {
+			t.Fatalf("i: %v, r: %v", i, r)
+		}
+	}
+
+	////////
+
+	tbCtx = newTableContextWithWhere(t, "db1", "tb1", "a >= 5", "id", "a")
+	for i := -100; i < 1000; i++ {
+		r, err := tbCtx.WhereTrue(buildColumnValues(i, i))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != (i >= 5) {
+			t.Fatalf("i: %v, r: %v", i, r)
+		}
+	}
+
+	////////
+	tbCtx = newTableContextWithWhere(t, "db1", "tb1", "a >= 5 or b <=5 ", "id", "a", "b")
+	{
+		a,b := 2,3
+		r, err := tbCtx.WhereTrue(buildColumnValues(1,2,3))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if r != (a >= 5 || b <= 5) {
+			t.Fatalf("r: %v", r)
 		}
 	}
 }
