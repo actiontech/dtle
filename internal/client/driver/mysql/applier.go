actiontech/dtle

commit ce1c811b8d847b7ff633c969a6fadbba7c1328d3

fix _ #333-2: drop schema vs session current schema
Since non-DML (DDL) is quite rare, it is acceptable to execute 'use <db>'
each time.

#174 related.

internal/client/driver/mysql/applier.go
@@ -1116,21 +1116,19 @@ func (a *Applier) ApplyBinlogEvent(workerIdx int, binlogEntry *binlog.BinlogEntr
 			binlogEntry.Coordinates.GNO, i)
 		switch event.DML {
 		case binlog.NotDML:
+			var err error
 			a.logger.Debugf("mysql.applier: ApplyBinlogEvent: not dml: %v", event.Query)
 
-			if event.CurrentSchema != "" && event.CurrentSchema != dbApplier.CurrentSchema {
-				query := fmt.Sprintf("USE %s", event.CurrentSchema)
-				a.logger.Debugf("mysql.applier: query: %v", query)
-				_, err := tx.Exec(query)
-				if err != nil {
-					if !sql.IgnoreError(err) {
-						a.logger.Errorf("mysql.applier: Exec sql error: %v", err)
-						return err
-					} else {
-						a.logger.Warnf("mysql.applier: Ignore error: %v", err)
-					}
+			// TODO escape schema name?
+			query := fmt.Sprintf("USE %s", event.CurrentSchema)
+			a.logger.Debugf("mysql.applier: query: %v", query)
+			_, err = tx.Exec(query)
+			if err != nil {
+				if !sql.IgnoreError(err) {
+					a.logger.Errorf("mysql.applier: Exec sql error: %v", err)
+					return err
 				} else {
-					dbApplier.CurrentSchema = event.CurrentSchema
+					a.logger.Warnf("mysql.applier: Ignore error: %v", err)
 				}
 			}
 
@@ -1155,7 +1153,7 @@ func (a *Applier) ApplyBinlogEvent(workerIdx int, binlogEntry *binlog.BinlogEntr
 				}
 			}
 
-			_, err := tx.Exec(event.Query)
+			_, err = tx.Exec(event.Query)
 			if err != nil {
 				if !sql.IgnoreError(err) {
 					a.logger.Errorf("mysql.applier: Exec sql error: %v", err)

internal/client/driver/mysql/sql/sqlutils.go
@@ -142,7 +142,6 @@ type Conn struct {
 	Db      *gosql.Conn
 	Fde     string
 
-	CurrentSchema        string
 	PsDeleteExecutedGtid *gosql.Stmt
 	PsInsertExecutedGtid *gosql.Stmt
 }

commit 56e5da95258aa3deddb45b9dd73cf7371de8bd43

fix a bug introduced in ce1c811
ignore 'use' query if event.CurrentSchema is empty

internal/client/driver/mysql/applier.go
@@ -1119,16 +1119,18 @@ func (a *Applier) ApplyBinlogEvent(workerIdx int, binlogEntry *binlog.BinlogEntr
 			var err error
 			a.logger.Debugf("mysql.applier: ApplyBinlogEvent: not dml: %v", event.Query)
 
-			// TODO escape schema name?
-			query := fmt.Sprintf("USE %s", event.CurrentSchema)
-			a.logger.Debugf("mysql.applier: query: %v", query)
-			_, err = tx.Exec(query)
-			if err != nil {
-				if !sql.IgnoreError(err) {
-					a.logger.Errorf("mysql.applier: Exec sql error: %v", err)
-					return err
-				} else {
-					a.logger.Warnf("mysql.applier: Ignore error: %v", err)
+			if event.CurrentSchema != "" {
+				// TODO escape schema name?
+				query := fmt.Sprintf("USE %s", event.CurrentSchema)
+				a.logger.Debugf("mysql.applier: query: %v", query)
+				_, err = tx.Exec(query)
+				if err != nil {
+					if !sql.IgnoreError(err) {
+						a.logger.Errorf("mysql.applier: Exec sql error: %v", err)
+						return err
+					} else {
+						a.logger.Warnf("mysql.applier: Ignore error: %v", err)
+					}
 				}
 			}
 