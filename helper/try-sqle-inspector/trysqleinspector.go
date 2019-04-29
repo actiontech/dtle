336c5bca810ceb23e79daa3c985b32dbf5a93b82
ffffwh <ffffwh@icloud.com>
2019-04-29 14:51:37 +0800

    Save table name in original case instead of lower case (#441)

--- helper/try-sqle-inspector/trysqleinspector.go
+++ helper/try-sqle-inspector/trysqleinspector.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"github.com/actiontech/dtle/internal/client/driver/mysql/base"
+	"github.com/actiontech/dtle/internal/client/driver/mysql/sqle/inspector"
 	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
 	"log"
-	"github.com/actiontech/dtle/internal/client/driver/mysql/sqle/inspector"
 )
 
 
@@ -22,11 +22,12 @@ func do(sql string) {
 
 func main() {
 	ctx.LoadSchemas(nil)
-	do("create schema a")
-	ctx.LoadTables("a", nil)
-	ctx.UseSchema("a")
+	//do("create schema a")
+	//ctx.LoadTables("a", nil)
+	//ctx.UseSchema("a")
 
-	case4()
+	case5()
+	//case4()
 	//case1()
 	//case2()
 	//case3()
@@ -37,6 +38,63 @@ func panicIfErr(err interface{}, args ...interface{}) {
 		log.Panicf("will panic. err %v, args: %v", err, args)
 	}
 }
+func case5() {
+	log.Printf("---- case 5")
+	do("create schema a")
+	//schemaName := "tomasdata_new"
+	schemaName := "a"
+	tableName := "HistoryAlarmData"
+	//tableName := "aaa"
+	ctx.LoadTables("a", nil)
+	ctx.UseSchema("a")
+
+	do("CREATE TABLE `HistoryAlarmData` (`product` varchar(15) NOT NULL, `partcode` varchar(12) NOT NULL, `boardtype` varchar(20) NOT NULL, `alarmtime` datetime NOT NULL, `alarmobjectid` int(7) NOT NULL, `recoverTime` datetime NOT NULL, `alarmcode` varchar(45) NOT NULL, `area` varchar(15) NOT NULL, `city` varchar(15) NOT NULL, `subnetid` int(15) NOT NULL, `recovertype` varchar(15) NOT NULL, `duration` time NOT NULL, `sitetype` varchar(10) NOT NULL, `networkelement` varchar(45) NOT NULL, `innerlocation` varchar(127) NOT NULL, `additionaltext` varchar(255) NOT NULL, PRIMARY KEY (`alarmtime`))")
+
+	tableInfo, exist := ctx.GetTable(schemaName, tableName)
+	if !exist {
+		panic("shoud exist")
+	}
+
+	cStmt := tableInfo.MergedTable
+	if cStmt == nil {
+		cStmt = tableInfo.OriginalTable
+	}
+
+	colList, err := base.GetTableColumnsSqle(ctx, schemaName, tableName)
+	if err != nil {
+		panicIfErr(err, "at GetTableColumnsSqle")
+	}
+	for _, col := range colList.ColumnList() {
+		log.Printf("col %v %v %v %v %v", col.Name, col.Type, col.IsPk(), col.Nullable, col.Default)
+	}
+
+	for _, col := range cStmt.Cols {
+		log.Printf("name %v tp %v", col.Name, col.Tp)
+		for _, opt := range col.Options {
+			switch opt.Tp {
+			case ast.ColumnOptionNoOption:
+			case ast.ColumnOptionPrimaryKey:
+				log.Printf("  pk")
+			case ast.ColumnOptionNotNull:
+				log.Printf("  not null")
+			case ast.ColumnOptionAutoIncrement:
+				log.Printf("  auto incr")
+			case ast.ColumnOptionDefaultValue:
+				log.Printf("  default %v", opt.Expr.Text())
+			case ast.ColumnOptionUniqKey:
+				log.Printf("  unique")
+			case ast.ColumnOptionNull:
+				log.Printf("  null")
+			case ast.ColumnOptionOnUpdate:
+			case ast.ColumnOptionFulltext:
+			case ast.ColumnOptionComment:
+			case ast.ColumnOptionGenerated:
+			case ast.ColumnOptionReference:
+			}
+		}
+	}
+}
+
 func case4() {
 	log.Printf("---- case 4")
 	do("create table a.a (id int primary key default 42, val1 varchar(50))")
