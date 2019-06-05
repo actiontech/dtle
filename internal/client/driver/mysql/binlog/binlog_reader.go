11772abc70be176cf76dbdd577f67f8ee98f4c32
lvhailong@actionsky.com <790493303@qq.com>
2019-06-05 14:52:37 +0800

    fix bug  auto test 130-2 case dead

--- internal/client/driver/mysql/binlog/binlog_reader.go
+++ internal/client/driver/mysql/binlog/binlog_reader.go
@@ -1375,7 +1375,7 @@ func (b *BinlogReader) matchDB(patternDBS []*config.DataSource, a string) bool {
 
 func (b *BinlogReader) matchTable(patternTBS []*config.DataSource, schemaName string, tableName string) bool {
 	for _, pdb := range patternTBS {
-		if (pdb.TableSchemaScope == "schema" || pdb.TableSchemaScope == "tables" || pdb.TableSchemaScope == "table") && schemaName == pdb.TableSchema && pdb.Tables == nil {
+		if (pdb.TableSchemaScope == "schema" || pdb.TableSchemaScope == "tables") && schemaName == pdb.TableSchema {
 			return true
 		}
 		redb, okdb := b.ReMap[pdb.TableSchema]
