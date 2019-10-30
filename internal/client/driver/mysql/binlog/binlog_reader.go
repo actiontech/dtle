actiontech/dtle

commit d5cec8e1945046057bc24a2ea5c84fb0b62aa5dc

Start relay from relay position GTID,
and start reading relaylog from *replay* position.

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -280,6 +280,17 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 			User:     b.mysqlContext.ConnectionConfig.User,
 			Password: b.mysqlContext.ConnectionConfig.Password,
 		}
+
+		var relayGtid string
+		if b.mysqlContext.RelayGtid == "" {
+			b.logger.WithField("gtid", coordinates.GtidSet).Debugf(
+				"ConnectBinlogStreamer. RelayGtid empty, use input gtid.")
+			relayGtid = coordinates.GtidSet
+		} else {
+			b.logger.WithField("gtid", b.mysqlContext.RelayGtid).Debugf(
+				"ConnectBinlogStreamer. use RelayGtid.")
+			relayGtid = b.mysqlContext.RelayGtid
+		}
 		relayConfig := &dmrelay.Config{
 			ServerID:   int(b.serverId),
 			Flavor:     "mysql",
@@ -287,7 +298,7 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 			RelayDir:   b.getBinlogDir(),
 			BinLogName: "",
 			EnableGTID: true,
-			BinlogGTID: coordinates.GtidSet,
+			BinlogGTID: relayGtid,
 		}
 		b.relay = dmrelay.NewRelay(relayConfig)
 		err = b.relay.Init()
@@ -296,7 +307,7 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 		}
 
 		meta := b.relay.GetMeta()
-		changed, err := meta.AdjustWithStartPos(coordinates.LogFile, coordinates.GtidSet, true)
+		changed, err := meta.AdjustWithStartPos(coordinates.LogFile, relayGtid, true)
 		if err != nil {
 			return err
 		}
@@ -316,7 +327,7 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 		}
 		b.binlogReader = streamer.NewBinlogReader(brConfig)
 
-		targetGtid, err := gtid.ParserGTID("mysql", coordinates.GtidSet)
+		targetGtid, err := gtid.ParserGTID("mysql", relayGtid)
 		if err != nil {
 			return err
 		}
@@ -330,8 +341,7 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 
 				// Since mysqlContext is passed into BinlogReader as a pointer, it will also
 				// modify extractor.mysqlContext.
-				b.mysqlContext.BinlogFile = p.Name
-				b.mysqlContext.BinlogPos = int64(p.Pos)
+				b.mysqlContext.RelayGtid = gs.String()
 
 				if waitForRelay {
 					if targetGtid.Contain(gs) {

internal/client/driver/mysql/extractor.go
@@ -1594,6 +1594,7 @@ func (e *Extractor) ID() string {
 			Gtid:                  e.mysqlContext.Gtid,
 			NatsAddr:              e.mysqlContext.NatsAddr,
 			ConnectionConfig:      e.mysqlContext.ConnectionConfig,
+			RelayGtid:             e.mysqlContext.RelayGtid,
 		},
 	}
 

internal/client/worker.go
@@ -187,6 +187,7 @@ func (r *Worker) SaveState() error {
 				tu.BinlogFile = id.DriverConfig.BinlogFile
 				tu.BinlogPos = id.DriverConfig.BinlogPos
 			} else { // TaskTypeSrc
+				tu.RelayGtid = id.DriverConfig.RelayGtid
 			}
 			r.workUpdates <- tu
 		}

internal/config/client.go
@@ -141,6 +141,7 @@ type MySQLDriverConfig struct {
 	GroupTimeout                        int // millisecond
 
 	Gtid                     string
+	RelayGtid                string
 	BinlogFile               string
 	BinlogPos                int64
 	GtidStart                string

internal/models/task.go
@@ -390,6 +390,7 @@ type TaskUpdate struct {
 	NatsAddr string
 	BinlogFile string
 	BinlogPos int64
+	RelayGtid string
 }
 
 const (

internal/server/fsm.go
@@ -528,14 +528,11 @@ func (n *udupFSM) applyJobClientUpdate(buf []byte, index uint64) interface{} {
 			existing.JobModifyIndex = index
 
 			for _, t := range existing.Tasks {
-				// GTID is updated regardless of task type
-				if ju.Gtid != "" {
-					n.logger.Debugf("*** write gtid %v", ju.Gtid)
-					t.Config["Gtid"] = ju.Gtid
-				}
-
-				if t.Type == ju.TaskType {
-					t.Config["NatsAddr"] = ju.NatsAddr
+				{ // these should be updated regardless of task type
+					if ju.Gtid != "" {
+						n.logger.Debugf("*** write gtid %v", ju.Gtid)
+						t.Config["Gtid"] = ju.Gtid
+					}
 
 					if ju.BinlogFile != "" {
 						n.logger.Debugf("*** udupFSM.applyJobClientUpdate. update BinlogFile %v", ju.BinlogFile)
@@ -544,6 +541,14 @@ func (n *udupFSM) applyJobClientUpdate(buf []byte, index uint64) interface{} {
 					if ju.BinlogPos != 0 {
 						n.logger.Debugf("*** udupFSM.applyJobClientUpdate. update BinlogPos %v", ju.BinlogPos)
 						t.Config["BinlogPos"] = ju.BinlogPos
+					}
+				}
+
+				if t.Type == ju.TaskType {
+					t.Config["NatsAddr"] = ju.NatsAddr
+
+					if ju.RelayGtid != "" {
+						t.Config["RelayGtid"] = ju.RelayGtid
 					}
 				}
 			}

commit dbb570a64bde99c8fe7763557f26dd511495135f

fix a bug introduced in d5cec8e1

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -328,7 +328,7 @@ func (b *BinlogReader) ConnectBinlogStreamer(coordinates base.BinlogCoordinatesX
 		}
 		b.binlogReader = streamer.NewBinlogReader(brConfig)
 
-		targetGtid, err := gtid.ParserGTID("mysql", relayGtid)
+		targetGtid, err := gtid.ParserGTID("mysql", coordinates.GtidSet)
 		if err != nil {
 			return err
 		}