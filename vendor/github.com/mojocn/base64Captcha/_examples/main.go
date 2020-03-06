dcd179ffe9cd730321fe3ee73f354bc8f9548b8c
lvhailong@actionsky.com <790493303@qq.com>
2020-03-06 21:16:58 +0800

    add package base64CaptchaV1.22

---
+++ vendor/github.com/mojocn/base64Captcha/_examples/main.go
@@ -0,0 +1,102 @@
+// example of HTTP server that uses the captcha package.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mojocn/base64Captcha"
+	"log"
+	"net/http"
+)
+
+//configJsonBody json request body.
+type configJsonBody struct {
+	Id              string
+	CaptchaType     string
+	VerifyValue     string
+	ConfigAudio     base64Captcha.ConfigAudio
+	ConfigCharacter base64Captcha.ConfigCharacter
+	ConfigDigit     base64Captcha.ConfigDigit
+}
+
+// base64Captcha create http handler
+func generateCaptchaHandler(w http.ResponseWriter, r *http.Request) {
+	//parse request parameters
+	//接收客户端发送来的请求参数
+	decoder := json.NewDecoder(r.Body)
+	var postParameters configJsonBody
+	err := decoder.Decode(&postParameters)
+	if err != nil {
+		log.Println(err)
+	}
+	defer r.Body.Close()
+
+	//create base64 encoding captcha
+	//创建base64图像验证码
+
+	var config interface{}
+	switch postParameters.CaptchaType {
+	case "audio":
+		config = postParameters.ConfigAudio
+	case "character":
+		config = postParameters.ConfigCharacter
+	default:
+		config = postParameters.ConfigDigit
+	}
+	captchaId, captcaInterfaceInstance := base64Captcha.GenerateCaptcha(postParameters.Id, config)
+	base64blob := base64Captcha.CaptchaWriteToBase64Encoding(captcaInterfaceInstance)
+
+	//or you can just write the captcha content to the httpResponseWriter.
+	//before you put the captchaId into the response COOKIE.
+	//captcaInterfaceInstance.WriteTo(w)
+
+	//set json response
+	//设置json响应
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	body := map[string]interface{}{"code": 1, "data": base64blob, "captchaId": captchaId, "msg": "success"}
+	json.NewEncoder(w).Encode(body)
+}
+
+// base64Captcha verify http handler
+func captchaVerifyHandle(w http.ResponseWriter, r *http.Request) {
+
+	//parse request parameters
+	//接收客户端发送来的请求参数
+	decoder := json.NewDecoder(r.Body)
+	var postParameters configJsonBody
+	err := decoder.Decode(&postParameters)
+	if err != nil {
+		log.Println(err)
+	}
+	defer r.Body.Close()
+	//verify the captcha
+	//比较图像验证码
+	verifyResult := base64Captcha.VerifyCaptcha(postParameters.Id, postParameters.VerifyValue)
+
+	//set json response
+	//设置json响应
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	body := map[string]interface{}{"code": "error", "data": "验证失败", "msg": "captcha failed"}
+	if verifyResult {
+		body = map[string]interface{}{"code": "success", "data": "验证通过", "msg": "captcha verified"}
+	}
+	json.NewEncoder(w).Encode(body)
+}
+
+//start a net/http server
+//启动golang net/http 服务器
+func main() {
+	//serve Vuejs+ElementUI+Axios Web Application
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	//api for create captcha
+	http.HandleFunc("/api/getCaptcha", generateCaptchaHandler)
+
+	//api for verify captcha
+	http.HandleFunc("/api/verifyCaptcha", captchaVerifyHandle)
+
+	fmt.Println("Server is at :8777")
+	if err := http.ListenAndServe(":8777", nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ vendor/github.com/mojocn/base64Captcha/random_math.go
@@ -0,0 +1,146 @@
+package base64Captcha
+
+import (
+	"fmt"
+	"image/color"
+	"math"
+	"math/rand"
+	"time"
+)
+
+//randFromStringArray: create random text from sourceStringArray, ensure sequenced
+func randFromStringArray(num int, sourceStringArray []string) string {
+	target_runes := []rune{}
+	for len(target_runes) < num {
+		ftarget := []rune(randStringArrayNext(sourceStringArray))
+		// 切除超过需要字符的部分
+		icut := num - len(target_runes)
+		if icut > len(ftarget) {
+			icut = len(ftarget)
+		}
+		target_runes = append(target_runes, ftarget[0:icut]...)
+	}
+	return string(target_runes)
+}
+
+//randRuneArrayNext: select random one from input
+func randStringArrayNext(input []string) string {
+	r := randSeed()
+	i := r.Intn(len(input))
+	return input[i]
+}
+
+//randText create random text. 生成随机文本.
+func randText(num int, sourceChars string) string {
+	itemrunes := []rune(sourceChars)
+	textNum := len(itemrunes)
+
+	text := ""
+	r := randSeed()
+	for i := 0; i < num; i++ {
+		text = text + string(itemrunes[r.Intn(textNum)])
+	}
+	return text
+}
+
+//RandArithmetic create random arithmetic equation and result.
+//穿件计算公式和返回结果
+func randArithmetic() (question, result string) {
+	operators := []string{"+", "-", "x"}
+	var mathResult int32
+	r := randSeed()
+	switch operators[r.Int31n(3)] {
+	case "+":
+		a := r.Int31n(100)
+		b := r.Int31n(100)
+		question = fmt.Sprintf("%d+%d=?", a, b)
+		mathResult = a + b
+	case "x":
+		a := r.Int31n(10)
+		b := r.Int31n(10)
+		question = fmt.Sprintf("%dx%d=?", a, b)
+		mathResult = a * b
+	default:
+		a := r.Int31n(100)
+		b := r.Int31n(100)
+		if a > b {
+			question = fmt.Sprintf("%d-%d=?", a, b)
+			mathResult = a - b
+		} else {
+			question = fmt.Sprintf("%d-%d=?", b, a)
+			mathResult = b - a
+		}
+	}
+	result = fmt.Sprintf("%d", mathResult)
+	return
+}
+
+//Random get random in min between max. 生成指定大小的随机数.
+func random(min int64, max int64) float64 {
+
+	r := randSeed()
+	if max <= min {
+		panic(fmt.Sprintf("invalid range %d >= %d", max, min))
+	}
+	decimal := r.Float64()
+
+	if max <= 0 {
+		return (float64(r.Int63n((min*-1)-(max*-1))+(max*-1)) + decimal) * -1
+	}
+	if min < 0 && max > 0 {
+		if r.Int()%2 == 0 {
+			return float64(r.Int63n(max)) + decimal
+		}
+		return (float64(r.Int63n(min*-1)) + decimal) * -1
+	}
+	return float64(r.Int63n(max-min)+min) + decimal
+}
+
+//randDeepColor get random deep color. 随机生成深色系.
+func randDeepColor() color.RGBA {
+	r := randSeed()
+
+	randColor := randColor()
+
+	increase := float64(30 + r.Intn(255))
+
+	red := math.Abs(math.Min(float64(randColor.R)-increase, 255))
+
+	green := math.Abs(math.Min(float64(randColor.G)-increase, 255))
+	blue := math.Abs(math.Min(float64(randColor.B)-increase, 255))
+
+	return color.RGBA{R: uint8(red), G: uint8(green), B: uint8(blue), A: uint8(255)}
+}
+
+//randLightColor get random ligth color. 随机生成浅色.
+func randLightColor() color.RGBA {
+	r := randSeed()
+
+	red := r.Intn(55) + 200
+	green := r.Intn(55) + 200
+	blue := r.Intn(55) + 200
+
+	return color.RGBA{R: uint8(red), G: uint8(green), B: uint8(blue), A: uint8(255)}
+}
+
+//randColor get random color. 生成随机颜色.
+func randColor() color.RGBA {
+	r := randSeed()
+
+	red := r.Intn(255)
+	green := r.Intn(255)
+	var blue int
+	if (red + green) > 400 {
+		blue = 0
+	} else {
+		blue = 400 - green - red
+	}
+	if blue > 255 {
+		blue = 255
+	}
+	return color.RGBA{R: uint8(red), G: uint8(green), B: uint8(blue), A: uint8(255)}
+}
+
+func randSeed() *rand.Rand {
+	return rand.New(rand.NewSource(time.Now().UnixNano()))
+}
