actiontech/dtle

commit 8f3311133828d849d8577cb2f2208f02d09c2aa5

column mapping #505. stage 1

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -796,6 +796,25 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					// decides whether action is taken sycnhronously (meaning we wait before
 					// next iteration) or asynchronously (we keep pushing more events)
 					// In reality, reads will be synchronous
+					if table != nil && table.Table.ColumnMap != nil {
+						if dmlEvent.NewColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.NewColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.NewColumnValues.AbstractValues = newRow
+						}
+
+						if dmlEvent.WhereColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.WhereColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.WhereColumnValues.AbstractValues = newRow
+						}
+					}
 					b.currentBinlogEntry.Events = append(b.currentBinlogEntry.Events, dmlEvent)
 				} else {
 					b.logger.Debugf("event has not passed 'where'")
@@ -1653,6 +1672,7 @@ func (b *BinlogReader) updateTableMeta(table *config.Table, realSchema string, t
 		table.Where = "true"
 	}
 	table.OriginalTableColumns = columns
+	table.ColumnMap = config.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
 	tableMap := b.getDbTableMap(realSchema)
 	err = b.addTableToTableMap(tableMap, table)
 	if err != nil {

internal/client/driver/mysql/dumper.go
@@ -63,6 +63,7 @@ func NewDumper(db usql.QueryAble, table *config.Table, chunkSize int64,
 		shutdownCh:         make(chan struct{}),
 		sentTableDef:       false,
 	}
+
 	switch os.Getenv(g.ENV_DUMP_CHECKSUM) {
 	case "1":
 		dumper.doChecksum = 1
@@ -312,6 +313,14 @@ func (d *dumper) getChunkData() (nRows int64, err error) {
 	}
 	if d.table.TableSchemaRename != "" {
 		entry.TableSchema = d.table.TableSchemaRename
+	}
+	for i, oldRow := range entry.ValuesX {
+		row := make([]*[]byte, len(d.table.ColumnMap))
+		for i := range d.table.ColumnMap {
+			fromIdx := d.table.ColumnMap[i]
+			row[i] = oldRow[fromIdx]
+		}
+		entry.ValuesX[i] = row
 	}
 	// ValuesX[i]: n-th row
 	// ValuesX[i][j]: j-th col of n-th row

internal/client/driver/mysql/extractor.go
@@ -407,7 +407,7 @@ func (e *Extractor) inspectTables() (err error) {
 				TableSchemaScope:       doDb.TableSchemaScope,
 				TableSchemaRenameRegex: doDb.TableSchemaRenameRegex,
 			}
-			if len(doDb.Tables) == 0 {
+			if len(doDb.Tables) == 0 { // replicate all tables
 				tbs, err := sql.ShowTables(e.db, doDb.TableSchema, e.mysqlContext.ExpandSyntaxSupport)
 				if err != nil {
 					return err
@@ -421,13 +421,14 @@ func (e *Extractor) inspectTables() (err error) {
 					}
 					db.Tables = append(db.Tables, doTb)
 				}
-			} else {
+			} else { // replicate selected tables
 				for _, doTb := range doDb.Tables {
 					doTb.TableSchema = doDb.TableSchema
 					doTb.TableSchemaRename = doDb.TableSchemaRename
 					if doTb.Where == "" {
 						doTb.Where = "true"
 					}
+
 					var regex string
 					if doTb.TableRegex != "" && doTb.TableName == "" && doTb.TableRename != "" {
 						regex = doTb.TableRegex
@@ -484,7 +485,7 @@ func (e *Extractor) inspectTables() (err error) {
 			e.replicateDoDb = append(e.replicateDoDb, db)
 		}
 		e.mysqlContext.ReplicateDoDb = e.replicateDoDb
-	} else {
+	} else { // empty DoDB. replicate all db/tb
 		dbs, err := sql.ShowDatabases(e.db)
 		if err != nil {
 			return err
@@ -540,6 +541,7 @@ func (e *Extractor) inspectTables() (err error) {
 
 	return nil
 }
+
 func (e *Extractor) ignoreDb(dbName string) bool {
 	for _, ignoreDb := range e.mysqlContext.ReplicateIgnoreDb {
 		if ignoreDb.TableSchema == dbName && len(ignoreDb.Tables) == 0 {
@@ -571,6 +573,8 @@ func (e *Extractor) readTableColumns() (err error) {
 			if err != nil {
 				return err
 			}
+			doTb.ColumnMap = config.BuildColumnMapIndex(doTb.ColumnMapFrom, doTb.OriginalTableColumns.Ordinals)
+
 		}
 	}
 	return nil

internal/client/driver/mysql/inspect.go
@@ -86,6 +86,9 @@ func (i *Inspector) ValidateOriginalTable(databaseName, tableName string, table 
 	if err != nil {
 		return err
 	}
+	// TODO why assign OriginalTableColumns twice (later getSchemaTablesAndMeta->readTableColumns)?
+	table.ColumnMap = uconf.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
+
 
 	i.logger.Debugf("table: %s.%s. n_unique_keys: %d", table.TableSchema, table.TableName, len(uniqueKeys))
 

internal/client/driver/mysql/sql/builder.go
@@ -222,6 +222,7 @@ func BuildDMLInsertQuery(databaseName, tableName string, tableColumns, sharedCol
 }
 
 func BuildDMLUpdateQuery(databaseName, tableName string, tableColumns, sharedColumns, mappedSharedColumns, uniqueKeyColumns *umconf.ColumnList, valueArgs, whereArgs []*interface{}) (result string, sharedArgs, columnArgs []interface{}, hasUK bool, err error) {
+
 	if len(valueArgs) < tableColumns.Len() {
 		return result, sharedArgs, columnArgs, hasUK, fmt.Errorf("value args count differs from table column count in BuildDMLUpdateQuery %v, %v",
 			len(valueArgs), tableColumns.Len())

internal/config/client.go
@@ -274,16 +274,29 @@ type Table struct {
 	TableSchema       string
 	TableSchemaRename string
 	Counter           int64
+	ColumnMapFrom     []string
+	//ColumnMapTo       []string
+	//ColumnMapUseRe    bool
 
 	OriginalTableColumns *umconf.ColumnList
 	UseUniqueKey         *umconf.UniqueKey
 	Iteration            int64
+	ColumnMap            []int
 
 	TableType    string
 	TableEngine  string
 	RowsEstimate int64
 
 	Where string // TODO load from job description
+}
+
+func BuildColumnMapIndex(from []string, ordinals umconf.ColumnsMap) (mapIndex []int) {
+	mapIndex = make([]int, len(from))
+	for i, colName := range from {
+		idxFrom := ordinals[colName]
+		mapIndex[i] = idxFrom
+	}
+	return mapIndex
 }
 
 type TableContext struct {

commit 3d923d226859e6ec3217472ce9cdbfcbbd484ebe

fix a bug introduced in 8f331113
correctly handle ColumnMap = nil

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -796,7 +796,7 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					// decides whether action is taken sycnhronously (meaning we wait before
 					// next iteration) or asynchronously (we keep pushing more events)
 					// In reality, reads will be synchronous
-					if table != nil && table.Table.ColumnMap != nil {
+					if table != nil && len(table.Table.ColumnMap) > 0 {
 						if dmlEvent.NewColumnValues != nil {
 							newRow := make([]*interface{}, len(table.Table.ColumnMap))
 							for i := range table.Table.ColumnMap {

internal/client/driver/mysql/dumper.go
@@ -314,13 +314,15 @@ func (d *dumper) getChunkData() (nRows int64, err error) {
 	if d.table.TableSchemaRename != "" {
 		entry.TableSchema = d.table.TableSchemaRename
 	}
-	for i, oldRow := range entry.ValuesX {
-		row := make([]*[]byte, len(d.table.ColumnMap))
-		for i := range d.table.ColumnMap {
-			fromIdx := d.table.ColumnMap[i]
-			row[i] = oldRow[fromIdx]
+	if len(d.table.ColumnMap) > 0 {
+		for i, oldRow := range entry.ValuesX {
+			row := make([]*[]byte, len(d.table.ColumnMap))
+			for i := range d.table.ColumnMap {
+				fromIdx := d.table.ColumnMap[i]
+				row[i] = oldRow[fromIdx]
+			}
+			entry.ValuesX[i] = row
 		}
-		entry.ValuesX[i] = row
 	}
 	// ValuesX[i]: n-th row
 	// ValuesX[i][j]: j-th col of n-th row