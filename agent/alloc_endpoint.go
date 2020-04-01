e9dbd3967ac8bfd42782a06557efa7c2785f0403
lvhailong@actionsky.com <790493303@qq.com>
2020-04-01 20:16:44 +0800

    fix bug bit err (job in different node,inc job was fail) fix bug   alloc status can not get in other node

--- agent/alloc_endpoint.go
+++ agent/alloc_endpoint.go
@@ -12,6 +12,9 @@ import (
 	"strings"
 
 	umodel "github.com/actiontech/dtle/internal/models"
+	"io/ioutil"
+	"encoding/json"
+	"strconv"
 )
 
 var (
@@ -87,25 +90,72 @@ func (s *HTTPServer) ClientAllocRequest(resp http.ResponseWriter, req *http.Requ
 	// tokenize the suffix of the path to get the alloc id and find the action
 	// invoked on the alloc id
 	tokens := strings.Split(reqSuffix, "/")
-	if len(tokens) != 2 {
+	var forward bool
+	if len(tokens) == 2{
+		forward=false
+	}else if len(tokens) ==3  {
+		forward=true
+	}else{
 		return nil, CodedError(404, resourceNotFoundErr)
 	}
+
 	allocID := tokens[0]
 	switch tokens[1] {
 	case "stats":
-		return s.allocStats(allocID, resp, req)
+		return s.allocStats(allocID,forward, resp, req)
 	}
-
 	return nil, CodedError(404, resourceNotFoundErr)
 }
 
-func (s *HTTPServer) allocStats(allocID string, resp http.ResponseWriter, req *http.Request) (interface{}, error) {
+func (s *HTTPServer) allocStats(allocID string,forward bool, resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	clientStats := s.agent.client.StatsReporter()
 	aStats, err := clientStats.GetAllocStats(allocID)
-	if err != nil {
+	if err != nil && strings.Contains(err.Error(), "unknown allocation ID") && !forward{
+		s.logger.Debug("not find alloc in %v,forward to other node to find ",s.addr)
+		 port:=s.agent.config.Ports.HTTP
+		 if port==0{
+			port=8190
+		}
+		return  s.allocStatsForward(allocID,s.addr,strconv.Itoa(port))
+	}else if  err != nil{
 		return nil, err
 	}
 
 	task := req.URL.Query().Get("task")
 	return aStats.LatestAllocStats(task)
 }
+func (s *HTTPServer) allocStatsForward(allocID string,addr string,port string) (interface{}, error) {
+
+	//get other node ip
+	args := &umodel.GenericRequest{}
+	var out umodel.ServerMembersResponse
+	if err := s.agent.RPC("Status.Members", args, &out); err != nil {
+		return nil, err
+	}
+	var realErr  error
+	//forward to other ip
+	for _ ,server:=range out.Members{
+		serverAddr:= server.Addr.String()+":"+port
+		if serverAddr!=addr{
+			url:="http://"+ serverAddr+"/v1/agent/allocation/"+allocID+"/stats/forward"
+			resp, err :=   http.Get(url)
+			if err != nil {
+				realErr= err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode == 200 {
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+				var value interface{}
+				if err:=json.Unmarshal(body,&value);err!=nil{
+					return  nil,err
+				}
+				return  value,nil
+			}
+		}
+	}
+	return nil,realErr
+}
+
