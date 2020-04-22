ebaba4518e2f69366d4b34edc1e52e02120d2fc6
ffffwh <ffffwh@icloud.com>
2020-04-22 16:48:11 +0800

    fix panic. caused by 2386f88

--- internal/client/driver/mysql/applier.go
+++ internal/client/driver/mysql/applier.go
@@ -636,8 +636,9 @@ func (a *Applier) updateGtidSet(sidStr string, sid uuid.UUID, txGno int64) {
 			SID:       sid,
 			Intervals: slice,
 		})
+	} else {
+		uuidSet.AddInterval(slice)
 	}
-	uuidSet.AddInterval(slice)
 }
 
 func (a *Applier) heterogeneousReplay() {
