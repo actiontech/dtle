actiontech/dtle

commit 8f3311133828d849d8577cb2f2208f02d09c2aa5

column mapping #505. stage 1

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -796,6 +796,25 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					// decides whether action is taken sycnhronously (meaning we wait before
 					// next iteration) or asynchronously (we keep pushing more events)
 					// In reality, reads will be synchronous
+					if table != nil && table.Table.ColumnMap != nil {
+						if dmlEvent.NewColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.NewColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.NewColumnValues.AbstractValues = newRow
+						}
+
+						if dmlEvent.WhereColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.WhereColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.WhereColumnValues.AbstractValues = newRow
+						}
+					}
 					b.currentBinlogEntry.Events = append(b.currentBinlogEntry.Events, dmlEvent)
 				} else {
 					b.logger.Debugf("event has not passed 'where'")
@@ -1653,6 +1672,7 @@ func (b *BinlogReader) updateTableMeta(table *config.Table, realSchema string, t
 		table.Where = "true"
 	}
 	table.OriginalTableColumns = columns
+	table.ColumnMap = config.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
 	tableMap := b.getDbTableMap(realSchema)
 	err = b.addTableToTableMap(tableMap, table)
 	if err != nil {

internal/client/driver/mysql/dumper.go
@@ -63,6 +63,7 @@ func NewDumper(db usql.QueryAble, table *config.Table, chunkSize int64,
 		shutdownCh:         make(chan struct{}),
 		sentTableDef:       false,
 	}
+
 	switch os.Getenv(g.ENV_DUMP_CHECKSUM) {
 	case "1":
 		dumper.doChecksum = 1
@@ -312,6 +313,14 @@ func (d *dumper) getChunkData() (nRows int64, err error) {
 	}
 	if d.table.TableSchemaRename != "" {
 		entry.TableSchema = d.table.TableSchemaRename
+	}
+	for i, oldRow := range entry.ValuesX {
+		row := make([]*[]byte, len(d.table.ColumnMap))
+		for i := range d.table.ColumnMap {
+			fromIdx := d.table.ColumnMap[i]
+			row[i] = oldRow[fromIdx]
+		}
+		entry.ValuesX[i] = row
 	}
 	// ValuesX[i]: n-th row
 	// ValuesX[i][j]: j-th col of n-th row

internal/client/driver/mysql/extractor.go
@@ -407,7 +407,7 @@ func (e *Extractor) inspectTables() (err error) {
 				TableSchemaScope:       doDb.TableSchemaScope,
 				TableSchemaRenameRegex: doDb.TableSchemaRenameRegex,
 			}
-			if len(doDb.Tables) == 0 {
+			if len(doDb.Tables) == 0 { // replicate all tables
 				tbs, err := sql.ShowTables(e.db, doDb.TableSchema, e.mysqlContext.ExpandSyntaxSupport)
 				if err != nil {
 					return err
@@ -421,13 +421,14 @@ func (e *Extractor) inspectTables() (err error) {
 					}
 					db.Tables = append(db.Tables, doTb)
 				}
-			} else {
+			} else { // replicate selected tables
 				for _, doTb := range doDb.Tables {
 					doTb.TableSchema = doDb.TableSchema
 					doTb.TableSchemaRename = doDb.TableSchemaRename
 					if doTb.Where == "" {
 						doTb.Where = "true"
 					}
+
 					var regex string
 					if doTb.TableRegex != "" && doTb.TableName == "" && doTb.TableRename != "" {
 						regex = doTb.TableRegex
@@ -484,7 +485,7 @@ func (e *Extractor) inspectTables() (err error) {
 			e.replicateDoDb = append(e.replicateDoDb, db)
 		}
 		e.mysqlContext.ReplicateDoDb = e.replicateDoDb
-	} else {
+	} else { // empty DoDB. replicate all db/tb
 		dbs, err := sql.ShowDatabases(e.db)
 		if err != nil {
 			return err
@@ -540,6 +541,7 @@ func (e *Extractor) inspectTables() (err error) {
 
 	return nil
 }
+
 func (e *Extractor) ignoreDb(dbName string) bool {
 	for _, ignoreDb := range e.mysqlContext.ReplicateIgnoreDb {
 		if ignoreDb.TableSchema == dbName && len(ignoreDb.Tables) == 0 {
@@ -571,6 +573,8 @@ func (e *Extractor) readTableColumns() (err error) {
 			if err != nil {
 				return err
 			}
+			doTb.ColumnMap = config.BuildColumnMapIndex(doTb.ColumnMapFrom, doTb.OriginalTableColumns.Ordinals)
+
 		}
 	}
 	return nil

internal/client/driver/mysql/inspect.go
@@ -86,6 +86,9 @@ func (i *Inspector) ValidateOriginalTable(databaseName, tableName string, table 
 	if err != nil {
 		return err
 	}
+	// TODO why assign OriginalTableColumns twice (later getSchemaTablesAndMeta->readTableColumns)?
+	table.ColumnMap = uconf.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
+
 
 	i.logger.Debugf("table: %s.%s. n_unique_keys: %d", table.TableSchema, table.TableName, len(uniqueKeys))
 

internal/client/driver/mysql/sql/builder.go
@@ -222,6 +222,7 @@ func BuildDMLInsertQuery(databaseName, tableName string, tableColumns, sharedCol
 }
 
 func BuildDMLUpdateQuery(databaseName, tableName string, tableColumns, sharedColumns, mappedSharedColumns, uniqueKeyColumns *umconf.ColumnList, valueArgs, whereArgs []*interface{}) (result string, sharedArgs, columnArgs []interface{}, hasUK bool, err error) {
+
 	if len(valueArgs) < tableColumns.Len() {
 		return result, sharedArgs, columnArgs, hasUK, fmt.Errorf("value args count differs from table column count in BuildDMLUpdateQuery %v, %v",
 			len(valueArgs), tableColumns.Len())

internal/config/client.go
@@ -274,16 +274,29 @@ type Table struct {
 	TableSchema       string
 	TableSchemaRename string
 	Counter           int64
+	ColumnMapFrom     []string
+	//ColumnMapTo       []string
+	//ColumnMapUseRe    bool
 
 	OriginalTableColumns *umconf.ColumnList
 	UseUniqueKey         *umconf.UniqueKey
 	Iteration            int64
+	ColumnMap            []int
 
 	TableType    string
 	TableEngine  string
 	RowsEstimate int64
 
 	Where string // TODO load from job description
+}
+
+func BuildColumnMapIndex(from []string, ordinals umconf.ColumnsMap) (mapIndex []int) {
+	mapIndex = make([]int, len(from))
+	for i, colName := range from {
+		idxFrom := ordinals[colName]
+		mapIndex[i] = idxFrom
+	}
+	return mapIndex
 }
 
 type TableContext struct {

commit fb58facb345da0e6c0340d97a440bceb46d36ae2

squashed commits from master
fix _ #499: BinlogEntry size should not be increased if event/row ignored.

(cherry picked from commit 141f2d08bddda10cab7a88b05c7738023fb8e98a)

temp fix for _#494: nil pointer defer crash

(cherry picked from commit 07b0341c8ef9c922b58c2510d40e74fd1268e57f)

update vendor/dm: suppressing some logs

(cherry picked from commit 73d572df7a44ec30b43fa63c8926199f9d7cbcd5)

relay: update BinlogFile/Pos along with Gtid

when full copy done on applier.

(cherry picked from commit b8b190d0ba22683ad274bfba1b35a5770144aa16)

refactoring: simplify ColumnValues

(cherry picked from commit 15168c9f0884e955af4b9ed16822dbab3830046f)

column mapping #505. stage 1

(cherry picked from commit 8f3311133828d849d8577cb2f2208f02d09c2aa5)

fix a bug introduced in 8f331113

correctly handle ColumnMap = nil

(cherry picked from commit 3d923d226859e6ec3217472ce9cdbfcbbd484ebe)

fix saving multiple GTIDSet (#271-3)

(cherry picked from commit 2386f88209f04178a4bba73caf55e7cda82dd8a6)

fix panic. caused by 2386f88

(cherry picked from commit ebaba4518e2f69366d4b34edc1e52e02120d2fc6)

go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/circonus-labs/circonus-gometrics v2.2.1+incompatible // indirect
 	github.com/circonus-labs/circonusllhist v0.1.0 // indirect
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548
 	github.com/cznic/parser v0.0.0-20181122101858-d773202d5b1f // indirect
 	github.com/cznic/strutil v0.0.0-20181122101858-275e90344537 // indirect
 	github.com/cznic/y v0.0.0-20181122101901-b05e8c2e8d7b // indirect

internal/client/driver/mysql/applier.go
@@ -246,6 +246,8 @@ type Applier struct {
 	nDumpEntry     int64
 
 	stubFullApplyDelay time.Duration
+
+	gtidSet *gomysql.MysqlGTIDSet
 }
 
 func NewApplier(ctx *common.ExecContext, cfg *config.MySQLDriverConfig, logger *logrus.Logger) (*Applier, error) {
@@ -275,6 +277,10 @@ func NewApplier(ctx *common.ExecContext, cfg *config.MySQLDriverConfig, logger *
 		waitCh:                  make(chan *models.WaitResult, 1),
 		shutdownCh:              make(chan struct{}),
 		printTps:                os.Getenv(g.ENV_PRINT_TPS) != "",
+	}
+	a.gtidSet, err = DtleParseMysqlGTIDSet(a.mysqlContext.Gtid)
+	if err != nil {
+		return nil, err
 	}
 	stubFullApplyDelayStr := os.Getenv(g.ENV_FULL_APPLY_DELAY)
 	if stubFullApplyDelayStr == "" {
@@ -443,7 +449,7 @@ func (a *Applier) executeWriteFuncs() {
 		}()
 	}
 
-	if a.mysqlContext.Gtid == "" {
+	if a.mysqlContext.Gtid == "" { // full copy
 		a.logger.Printf("mysql.applier: Operating until row copy is complete")
 		a.mysqlContext.Stage = models.StageSlaveWaitingForWorkersToProcessQueue
 		for {
@@ -451,7 +457,13 @@ func (a *Applier) executeWriteFuncs() {
 				a.rowCopyComplete <- true
 				a.logger.Printf("mysql.applier: Rows copy complete.number of rows:%d", a.mysqlContext.TotalRowsReplay)
 				a.mysqlContext.Gtid = a.currentCoordinates.RetrievedGtidSet
+				var err error
+				a.gtidSet, err = DtleParseMysqlGTIDSet(a.mysqlContext.Gtid)
+				if err != nil {
+					a.onError(TaskStateDead, err)
+				}
 				a.mysqlContext.BinlogFile = a.currentCoordinates.File
+				a.mysqlContext.BinlogPos = a.currentCoordinates.Position
 				break
 			}
 			if a.shutdown {
@@ -487,20 +499,8 @@ func (a *Applier) executeWriteFuncs() {
 
 			if !a.shutdown {
 				a.lastAppliedBinlogTx = groupTx[len(groupTx)-1]
-				if a.mysqlContext.Gtid != "" {
-					sp := strings.Split(a.mysqlContext.Gtid, ",")
-
-					if strings.Split(sp[len(sp)-1], ":")[0] == a.lastAppliedBinlogTx.SID || strings.Split(sp[len(sp)-1], ":")[0] == "\n"+a.lastAppliedBinlogTx.SID {
-						sp[len(sp)-1] = fmt.Sprintf("%s:1-%d", a.lastAppliedBinlogTx.SID, a.lastAppliedBinlogTx.GNO)
-					}
-					var rgtid string
-					for _, gtid := range sp {
-						rgtid = rgtid + "," + gtid
-					}
-					a.mysqlContext.Gtid = rgtid[1:]
-				} else {
-					a.mysqlContext.Gtid = fmt.Sprintf("%s:1-%d", a.lastAppliedBinlogTx.SID, a.lastAppliedBinlogTx.GNO)
-				}
+				//a.updateGtidSet(a.lastAppliedBinlogTx.SID, TODO, ) // homogeneous obsolete. not implementing
+				a.updateGtidString()
 				// a.mysqlContext.BinlogPos = // homogeneous obsolete. not implementing.
 			}
 		case <-time.After(1 * time.Second):
@@ -614,6 +614,33 @@ func (a *Applier) cleanGtidExecuted(sid uuid.UUID, intervalStr string) error {
 		return err
 	}
 	return nil
+}
+
+func DtleParseMysqlGTIDSet(gtidSetStr string) (*gomysql.MysqlGTIDSet, error) {
+	set0, err := gomysql.ParseMysqlGTIDSet(gtidSetStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return set0.(*gomysql.MysqlGTIDSet), nil
+}
+
+func (a *Applier) updateGtidSet(sidStr string, sid uuid.UUID, txGno int64) {
+	slice := gomysql.IntervalSlice{gomysql.Interval{
+		Start: txGno,
+		Stop:  txGno + 1,
+	}}
+
+	// It seems they all use lower case for uuid.
+	uuidSet, ok := a.gtidSet.Sets[sidStr]
+	if !ok {
+		a.gtidSet.AddSet(&gomysql.UUIDSet{
+			SID:       sid,
+			Intervals: slice,
+		})
+	} else {
+		uuidSet.AddInterval(slice)
+	}
 }
 
 func (a *Applier) heterogeneousReplay() {
@@ -753,20 +780,8 @@ func (a *Applier) heterogeneousReplay() {
 			}
 			span.Finish()
 			if !a.shutdown {
-				// TODO what is this used for?
-				if a.mysqlContext.Gtid != "" {
-					sp := strings.Split(a.mysqlContext.Gtid, ",")
-					if strings.Split(sp[len(sp)-1], ":")[0] == txSid || strings.Split(sp[len(sp)-1], ":")[0] == "\n"+txSid {
-						sp[len(sp)-1] = fmt.Sprintf("%s:1-%d", txSid, binlogEntry.Coordinates.GNO)
-					}
-					var rgtid string
-					for _, gtid := range sp {
-						rgtid = rgtid + "," + gtid
-					}
-					a.mysqlContext.Gtid = rgtid[1:]
-				} else {
-					a.mysqlContext.Gtid = fmt.Sprintf("%s:1-%d", txSid, binlogEntry.Coordinates.GNO)
-				}
+				a.updateGtidSet(txSid, binlogEntry.Coordinates.SID, binlogEntry.Coordinates.GNO)
+				a.updateGtidString()
 				if a.mysqlContext.BinlogFile != binlogEntry.Coordinates.LogFile {
 					a.mysqlContext.BinlogFile = binlogEntry.Coordinates.LogFile
 					a.publishProgress()
@@ -803,19 +818,8 @@ OUTER:
 
 				if !a.shutdown {
 					a.lastAppliedBinlogTx = binlogTx
-					if a.mysqlContext.Gtid != "" {
-						sp := strings.Split(a.mysqlContext.Gtid, ",")
-						if strings.Split(sp[len(sp)-1], ":")[0] == a.lastAppliedBinlogTx.SID || strings.Split(sp[len(sp)-1], ":")[0] == "\n"+a.lastAppliedBinlogTx.SID {
-							sp[len(sp)-1] = fmt.Sprintf("%s:1-%d", a.lastAppliedBinlogTx.SID, a.lastAppliedBinlogTx.GNO)
-						}
-						var rgtid string
-						for _, gtid := range sp {
-							rgtid = rgtid + "," + gtid
-						}
-						a.mysqlContext.Gtid = rgtid[1:]
-					} else {
-						a.mysqlContext.Gtid = fmt.Sprintf("%s:1-%d", a.lastAppliedBinlogTx.SID, a.lastAppliedBinlogTx.GNO)
-					}
+					//a.updateGtidSet(a.lastAppliedBinlogTx.SID, TODO, a.lastAppliedBinlogTx.GNO)
+					a.updateGtidString()
 					// a.mysqlContext.BinlogPos = // homogeneous obsolete. not implementing.
 				}
 			} else {
@@ -903,6 +907,9 @@ func (a *Applier) initiateStreaming() error {
 			a.onError(TaskStateDead, err)
 		}
 		a.currentCoordinates.RetrievedGtidSet = dumpData.Gtid
+		a.currentCoordinates.File = dumpData.LogFile
+		a.currentCoordinates.Position = dumpData.LogPos
+
 		a.mysqlContext.Stage = models.StageSlaveWaitingForWorkersToProcessQueue
 
 		for atomic.LoadInt64(&a.nDumpEntry) != 0 {
@@ -1731,6 +1738,11 @@ func (a *Applier) ID() string {
 		a.logger.Errorf("mysql.applier: Failed to marshal ID to JSON: %s", err)
 	}
 	return string(data)
+}
+
+func (a *Applier) updateGtidString() {
+	a.mysqlContext.Gtid = a.gtidSet.String()
+	a.logger.Debugf("applier updateGtidString %v", a.mysqlContext.Gtid)
 }
 
 func (a *Applier) onError(state int, err error) {

internal/client/driver/mysql/binlog/binlog_entry.go
@@ -34,7 +34,7 @@ func NewBinlogEntryAt(coordinates base.BinlogCoordinateTx) *BinlogEntry {
 	binlogEntry := &BinlogEntry{
 		Coordinates:  coordinates,
 		Events:       make([]DataEvent, 0),
-		OriginalSize: 0,
+		OriginalSize: 1, // GroupMaxSize is default to 1 and we send on EntriesSize >= GroupMaxSize
 	}
 	return binlogEntry
 }

internal/client/driver/mysql/binlog/binlog_reader.go
@@ -398,7 +398,6 @@ func (b *BinlogReader) GetCurrentBinlogCoordinates() *base.BinlogCoordinateTx {
 func ToColumnValuesV2(abstractValues []interface{}, table *config.TableContext) *mysql.ColumnValues {
 	result := &mysql.ColumnValues{
 		AbstractValues: make([]*interface{}, len(abstractValues)),
-		ValuesPointers: make([]*interface{}, len(abstractValues)),
 	}
 
 	for i := 0; i < len(abstractValues); i++ {
@@ -423,7 +422,6 @@ func ToColumnValuesV2(abstractValues []interface{}, table *config.TableContext) 
 			}
 		}
 		result.AbstractValues[i] = &abstractValues[i]
-		result.ValuesPointers[i] = result.AbstractValues[i]
 	}
 
 	return result
@@ -447,12 +445,6 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 	if b.currentCoordinates.SmallerThanOrEquals(&b.LastAppliedRowsEventHint) {
 		b.logger.Debugf("mysql.reader: Skipping handled query at %+v", b.currentCoordinates)
 		return nil
-	}
-
-	// b.currentBinlogEntry is created on GtidEvent
-	// Size of GtidEvent is ignored.
-	if b.currentBinlogEntry != nil {
-		b.currentBinlogEntry.OriginalSize += len(ev.RawData)
 	}
 
 	switch ev.Header.EventType {
@@ -516,6 +508,7 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					b.mysqlContext.SrcBinlogTimestamp = evt.ExecutionTime
 					b.currentBinlogEntry.Events = append(b.currentBinlogEntry.Events, event)
 					b.currentBinlogEntry.SpanContext = span.Context()
+					b.currentBinlogEntry.OriginalSize += len(ev.RawData)
 					entriesChannel <- b.currentBinlogEntry
 					b.LastAppliedRowsEventHint = b.currentCoordinates
 					return nil
@@ -660,6 +653,7 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					}
 				}
 				b.currentBinlogEntry.SpanContext = span.Context()
+				b.currentBinlogEntry.OriginalSize += len(ev.RawData)
 				entriesChannel <- b.currentBinlogEntry
 				b.LastAppliedRowsEventHint = b.currentCoordinates
 			}
@@ -716,6 +710,9 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 				return err
 			}
 			dmlEvent.OriginalTableColumns = originalTableColumns*/
+
+			// It is hard to calculate exact row size. We use estimation.
+			avgRowSize := len(ev.RawData) / len(rowsEvent.Rows)
 
 			for i, row := range rowsEvent.Rows {
 				b.logger.Debugf("mysql.reader: row values: %v", row[:mathutil.Min(len(row), g.LONG_LOG_LIMIT)])
@@ -795,10 +792,35 @@ func (b *BinlogReader) handleEvent(ev *replication.BinlogEvent, entriesChannel c
 					dmlEvent.DatabaseName = schema.TableSchemaRename
 				}
 				if whereTrue {
+					if dmlEvent.WhereColumnValues != nil {
+						b.currentBinlogEntry.OriginalSize += avgRowSize
+					}
+					if dmlEvent.NewColumnValues != nil {
+						b.currentBinlogEntry.OriginalSize += avgRowSize
+					}
 					// The channel will do the throttling. Whoever is reding from the channel
 					// decides whether action is taken sycnhronously (meaning we wait before
 					// next iteration) or asynchronously (we keep pushing more events)
 					// In reality, reads will be synchronous
+					if table != nil && len(table.Table.ColumnMap) > 0 {
+						if dmlEvent.NewColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.NewColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.NewColumnValues.AbstractValues = newRow
+						}
+
+						if dmlEvent.WhereColumnValues != nil {
+							newRow := make([]*interface{}, len(table.Table.ColumnMap))
+							for i := range table.Table.ColumnMap {
+								idx := table.Table.ColumnMap[i]
+								newRow[i] = dmlEvent.WhereColumnValues.AbstractValues[idx]
+							}
+							dmlEvent.WhereColumnValues.AbstractValues = newRow
+						}
+					}
 					b.currentBinlogEntry.Events = append(b.currentBinlogEntry.Events, dmlEvent)
 				} else {
 					b.logger.Debugf("event has not passed 'where'")
@@ -1450,7 +1472,7 @@ func (b *BinlogReader) skipRowEvent(rowsEvent *replication.RowsEvent, dml EventD
 			// We make no special treat for case 2. That tx has only one insert, which should be ignored.
 			if dml == InsertDML {
 				if len(rowsEvent.Rows) == 1 {
-					sidValue := *mysql.ToColumnValues(rowsEvent.Rows[0]).AbstractValues[1]
+					sidValue := rowsEvent.Rows[0][1]
 					sidByte, ok := sidValue.(string)
 					if !ok {
 						b.logger.Errorf("cycle-prevention: unrecognized gtid_executed table sid type: %T", sidValue)
@@ -1656,6 +1678,7 @@ func (b *BinlogReader) updateTableMeta(table *config.Table, realSchema string, t
 		table.Where = "true"
 	}
 	table.OriginalTableColumns = columns
+	table.ColumnMap = config.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
 	tableMap := b.getDbTableMap(realSchema)
 	err = b.addTableToTableMap(tableMap, table)
 	if err != nil {

internal/client/driver/mysql/dumper.go
@@ -63,6 +63,7 @@ func NewDumper(db usql.QueryAble, table *config.Table, chunkSize int64,
 		shutdownCh:         make(chan struct{}),
 		sentTableDef:       false,
 	}
+
 	switch os.Getenv(g.ENV_DUMP_CHECKSUM) {
 	case "1":
 		dumper.doChecksum = 1
@@ -80,9 +81,9 @@ func NewDumper(db usql.QueryAble, table *config.Table, chunkSize int64,
 
 type dumpStatResult struct {
 	Gtid       string
+	TotalCount int64
 	LogFile    string
 	LogPos     int64
-	TotalCount int64
 }
 
 type DumpEntryOrig struct {
@@ -312,6 +313,16 @@ func (d *dumper) getChunkData() (nRows int64, err error) {
 	}
 	if d.table.TableSchemaRename != "" {
 		entry.TableSchema = d.table.TableSchemaRename
+	}
+	if len(d.table.ColumnMap) > 0 {
+		for i, oldRow := range entry.ValuesX {
+			row := make([]*[]byte, len(d.table.ColumnMap))
+			for i := range d.table.ColumnMap {
+				fromIdx := d.table.ColumnMap[i]
+				row[i] = oldRow[fromIdx]
+			}
+			entry.ValuesX[i] = row
+		}
 	}
 	// ValuesX[i]: n-th row
 	// ValuesX[i][j]: j-th col of n-th row

internal/client/driver/mysql/extractor.go
@@ -411,7 +411,7 @@ func (e *Extractor) inspectTables() (err error) {
 				TableSchemaScope:       doDb.TableSchemaScope,
 				TableSchemaRenameRegex: doDb.TableSchemaRenameRegex,
 			}
-			if len(doDb.Tables) == 0 {
+			if len(doDb.Tables) == 0 { // replicate all tables
 				tbs, err := sql.ShowTables(e.db, doDb.TableSchema, e.mysqlContext.ExpandSyntaxSupport)
 				if err != nil {
 					return err
@@ -425,13 +425,14 @@ func (e *Extractor) inspectTables() (err error) {
 					}
 					db.Tables = append(db.Tables, doTb)
 				}
-			} else {
+			} else { // replicate selected tables
 				for _, doTb := range doDb.Tables {
 					doTb.TableSchema = doDb.TableSchema
 					doTb.TableSchemaRename = doDb.TableSchemaRename
 					if doTb.Where == "" {
 						doTb.Where = "true"
 					}
+
 					var regex string
 					if doTb.TableRegex != "" && doTb.TableName == "" && doTb.TableRename != "" {
 						regex = doTb.TableRegex
@@ -488,7 +489,7 @@ func (e *Extractor) inspectTables() (err error) {
 			e.replicateDoDb = append(e.replicateDoDb, db)
 		}
 		e.mysqlContext.ReplicateDoDb = e.replicateDoDb
-	} else {
+	} else { // empty DoDB. replicate all db/tb
 		dbs, err := sql.ShowDatabases(e.db)
 		if err != nil {
 			return err
@@ -544,6 +545,7 @@ func (e *Extractor) inspectTables() (err error) {
 
 	return nil
 }
+
 func (e *Extractor) ignoreDb(dbName string) bool {
 	for _, ignoreDb := range e.mysqlContext.ReplicateIgnoreDb {
 		if ignoreDb.TableSchema == dbName && len(ignoreDb.Tables) == 0 {
@@ -575,6 +577,8 @@ func (e *Extractor) readTableColumns() (err error) {
 			if err != nil {
 				return err
 			}
+			doTb.ColumnMap = config.BuildColumnMapIndex(doTb.ColumnMapFrom, doTb.OriginalTableColumns.Ordinals)
+
 		}
 	}
 	return nil

internal/client/driver/mysql/inspect.go
@@ -86,6 +86,9 @@ func (i *Inspector) ValidateOriginalTable(databaseName, tableName string, table 
 	if err != nil {
 		return err
 	}
+	// TODO why assign OriginalTableColumns twice (later getSchemaTablesAndMeta->readTableColumns)?
+	table.ColumnMap = uconf.BuildColumnMapIndex(table.ColumnMapFrom, table.OriginalTableColumns.Ordinals)
+
 
 	i.logger.Debugf("table: %s.%s. n_unique_keys: %d", table.TableSchema, table.TableName, len(uniqueKeys))
 

internal/client/driver/mysql/sql/builder.go
@@ -222,6 +222,7 @@ func BuildDMLInsertQuery(databaseName, tableName string, tableColumns, sharedCol
 }
 
 func BuildDMLUpdateQuery(databaseName, tableName string, tableColumns, sharedColumns, mappedSharedColumns, uniqueKeyColumns *umconf.ColumnList, valueArgs, whereArgs []*interface{}) (result string, sharedArgs, columnArgs []interface{}, hasUK bool, err error) {
+
 	if len(valueArgs) < tableColumns.Len() {
 		return result, sharedArgs, columnArgs, hasUK, fmt.Errorf("value args count differs from table column count in BuildDMLUpdateQuery %v, %v",
 			len(valueArgs), tableColumns.Len())

internal/config/client.go
@@ -276,16 +276,29 @@ type Table struct {
 	TableSchema       string
 	TableSchemaRename string
 	Counter           int64
+	ColumnMapFrom     []string
+	//ColumnMapTo       []string
+	//ColumnMapUseRe    bool
 
 	OriginalTableColumns *umconf.ColumnList
 	UseUniqueKey         *umconf.UniqueKey
 	Iteration            int64
+	ColumnMap            []int
 
 	TableType    string
 	TableEngine  string
 	RowsEstimate int64
 
 	Where string // TODO load from job description
+}
+
+func BuildColumnMapIndex(from []string, ordinals umconf.ColumnsMap) (mapIndex []int) {
+	mapIndex = make([]int, len(from))
+	for i, colName := range from {
+		idxFrom := ordinals[colName]
+		mapIndex[i] = idxFrom
+	}
+	return mapIndex
 }
 
 type TableContext struct {
@@ -314,13 +327,13 @@ func NewTable(schemaName string, tableName string) *Table {
 func (t *TableContext) WhereTrue(values *umconf.ColumnValues) (bool, error) {
 	var m = make(map[string]interface{})
 	for field, idx := range t.WhereCtx.FieldsMap {
-		nCols := len(values.ValuesPointers)
+		nCols := len(values.AbstractValues)
 		if idx >= nCols {
 			return false, fmt.Errorf("cannot eval 'where' predicate: no enough columns (%v < %v)", nCols, idx)
 		}
 
 		//fmt.Printf("**** type of %v %T\n", field, *values.ValuesPointers[idx])
-		rawValue := *(values.ValuesPointers[idx])
+		rawValue := *(values.AbstractValues[idx])
 		var value interface{}
 		if rawValue == nil {
 			value = rawValue

internal/config/mysql/types.go
@@ -321,25 +321,10 @@ func (c *UniqueKey) String() string {
 
 type ColumnValues struct {
 	AbstractValues []*interface{}
-	ValuesPointers []*interface{}
 }
 
 func (this *ColumnValues) GetAbstractValues() []*interface{} {
 	return this.AbstractValues
-}
-
-func ToColumnValues(abstractValues []interface{}) *ColumnValues {
-	result := &ColumnValues{
-		AbstractValues: make([]*interface{}, len(abstractValues)),
-		ValuesPointers: make([]*interface{}, len(abstractValues)),
-	}
-
-	for i := 0; i < len(abstractValues); i++ {
-		result.AbstractValues[i] = &abstractValues[i]
-		result.ValuesPointers[i] = result.AbstractValues[i]
-	}
-
-	return result
 }
 
 func (c *ColumnValues) StringColumn(index int) string {

internal/server/fsm.go
@@ -337,7 +337,15 @@ func (n *udupFSM) applyStatusUpdate(buf []byte, index uint64) interface{} {
 
 							}
 							if node.Status == models.NodeStatusDown {
-								alloc.TaskStates[alloc.Task].State = models.TaskStateDead
+								if alloc.TaskStates != nil { // #494
+									if alloc.TaskStates[alloc.Task] != nil {
+										alloc.TaskStates[alloc.Task].State = models.TaskStateDead
+									} else {
+										n.logger.Warnf("DTLE_BUG alloc.TaskStates[alloc.Task] == nil")
+									}
+								} else {
+									n.logger.Warnf("DTLE_BUG alloc.TaskStates == nil")
+								}
 								if err := n.state.UpsertAlloc(index, alloc); err != nil {
 									n.logger.Errorf("server.fsm: UpsertAlloc failed: %v", err)
 									return err

vendor/github.com/pingcap/dm/relay/metrics.go
@@ -138,7 +138,7 @@ func RegisterMetrics(registry *prometheus.Registry) {
 	registry.MustRegister(relayExitWithErrorCounter)
 }
 
-func reportRelayLogSpaceInBackground(dirpath string) error {
+func reportRelayLogSpaceInBackground(dirpath string, shutdown chan struct{}) error {
 	if len(dirpath) == 0 {
 		return errors.New("dirpath is empty")
 	}
@@ -148,9 +148,16 @@ func reportRelayLogSpaceInBackground(dirpath string) error {
 		defer ticker.Stop()
 
 		for range ticker.C {
+			select {
+			case <-shutdown:
+				log.Infof("reportRelayLogSpaceInBackground: shutdown")
+				return
+			default:
+			}
+
 			size, err := utils.GetStorageSize(dirpath)
 			if err != nil {
-				log.Error("update sotrage size err: ", err)
+				log.Error("update storage size err: ", err)
 			} else {
 				relayLogSpaceGauge.WithLabelValues("capacity").Set(float64(size.Capacity))
 				relayLogSpaceGauge.WithLabelValues("available").Set(float64(size.Available))

vendor/github.com/pingcap/dm/relay/relay.go
@@ -101,6 +101,7 @@ type Relay struct {
 
 	meta   Meta
 	closed sync2.AtomicBool
+	shutdown chan struct{}
 	sync.RWMutex
 
 	activeRelayLog struct {
@@ -145,6 +146,7 @@ func NewRealRelay(cfg *Config) Process {
 		cfg:       cfg,
 		syncerCfg: syncerCfg,
 		meta:      NewLocalMeta(cfg.Flavor, cfg.RelayDir),
+		shutdown:  make(chan struct{}),
 	}
 }
 
@@ -175,7 +177,7 @@ func (r *Relay) Init() (err error) {
 		return errors.Trace(err)
 	}
 
-	if err := reportRelayLogSpaceInBackground(r.cfg.RelayDir); err != nil {
+	if err := reportRelayLogSpaceInBackground(r.cfg.RelayDir, r.shutdown); err != nil {
 		return errors.Trace(err)
 	}
 
@@ -602,6 +604,8 @@ func (r *Relay) Close() {
 		return
 	}
 	log.Info("[relay] relay unit is closing")
+
+	close(r.shutdown)
 
 	r.stopSync()
 

internal/config/where_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/actiontech/dts/internal/client/driver/mysql/binlog"
 	"github.com/actiontech/dts/internal/config/mysql"
 	"testing"
 )
@@ -18,7 +19,7 @@ func newTableContextWithWhere(t *testing.T,
 	return tbCtx
 }
 func buildColumnValues(vals ...interface{}) *mysql.ColumnValues {
-	return mysql.ToColumnValues(vals)
+	return binlog.ToColumnValuesV2(vals, nil)
 }
 
 func TestWhereTrue(t *testing.T) {