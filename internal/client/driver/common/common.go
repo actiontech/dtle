9e376634186c316a606f842b66cdf0e4ce31dc20
ffffwh <ffffwh@icloud.com>
2020-07-06 12:10:00 +0800

    refactoring: extract functions about GtidSet

--- internal/client/driver/common/common.go
+++ internal/client/driver/common/common.go
@@ -1,8 +1,41 @@
 package common
 
+import (
+	"github.com/satori/go.uuid"
+	"github.com/siddontang/go-mysql/mysql"
+)
+
 type ExecContext struct {
 	Subject    string
 	Tp         string
 	MaxPayload int
 	StateDir   string
 }
+
+func DtleParseMysqlGTIDSet(gtidSetStr string) (*mysql.MysqlGTIDSet, error) {
+	set0, err := mysql.ParseMysqlGTIDSet(gtidSetStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return set0.(*mysql.MysqlGTIDSet), nil
+}
+
+func UpdateGtidSet(gtidSet *mysql.MysqlGTIDSet, sidStr string, sid uuid.UUID, txGno int64) {
+	slice := mysql.IntervalSlice{mysql.Interval{
+		Start: txGno,
+		Stop:  txGno + 1,
+	}}
+
+	// It seems they all use lower case for uuid.
+	uuidSet, ok := gtidSet.Sets[sidStr]
+	if !ok {
+		gtidSet.AddSet(&mysql.UUIDSet{
+			SID:       sid,
+			Intervals: slice,
+		})
+	} else {
+		uuidSet.AddInterval(slice)
+	}
+}
+
