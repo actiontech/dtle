de873207ce5012687f8724f3a88f10468623cfa2
ffffwh <ffffwh@icloud.com>
2020-08-14 17:52:08 +0800

    fix bug 1900year. timezone change. LMT (Local Mean Time) changed to CST (China Standard Time). +8:05:43 changed to +8:00:00
    pick ed1d2f0d

--- drivers/mysql/kafka/kafka2.go
+++ drivers/mysql/kafka/kafka2.go
@@ -443,6 +443,11 @@ func DateTimeValue(dateTime string, timeZone string) int64 {
 	if err != nil {
 		return 0
 	}
+	if tm2.UnixNano()/ 1e6 < -2177481600000{
+		return tm2.UnixNano() / 1e6 - 343000
+		// fix bug 1900 year. timezone change. LMT（Local Mean Time）change to CST (China Standard Time).
+		// +8:05:43 change to +8:00:00
+	}
 	return tm2.UnixNano() / 1e6
 }
 func DateValue(date string, timeZone string) int64 {
