80c55eb4f6859ffd9ee4f251a96b7e7c7314e40b
ffffwh <ffffwh@icloud.com>
2020-09-04 14:50:16 +0800

    adjust log. print version.

--- cmd/nomad-plugin/main.go
+++ cmd/nomad-plugin/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"github.com/hashicorp/nomad/plugins"
-	"github.com/hashicorp/go-hclog"
-	"os"
+	"flag"
+	"fmt"
 	dtle "github.com/actiontech/dtle/drivers/mysql"
+	"github.com/actiontech/dtle/g"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/plugins"
+	"os"
 )
 
 func main() {
+	versionStr := fmt.Sprintf("%v-%v-%v", g.Version, g.GitBranch, g.GitCommit)
+
+	isVersion := flag.Bool("version", false, "Show version")
+	flag.Parse()
+	if *isVersion {
+		fmt.Println(versionStr)
+		return
+	}
+
 	pid := os.Getpid()
 	plugins.Serve(func(logger hclog.Logger) interface{} {
-		loggerPid := logger.With("pid", pid)
-		loggerPid.Info("env", "GODEBUG", os.Getenv("GODEBUG"))
-		loggerPid.Warn("plugins.Serve Factory called.")
-		return dtle.NewDriver(loggerPid)
+		logger.Info("dtle starting", "version", versionStr, "pid", pid)
+		logger.Info("env", "GODEBUG", os.Getenv("GODEBUG"))
+		logger.Debug("plugins.Serve Factory called.")
+		return dtle.NewDriver(logger)
 	})
 }
