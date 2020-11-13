4028822361deadcd552980e45994915c4685bd07
luowei <luowei@actiontech.sky>
2020-11-13 19:05:23 +0800

    fix display of the job start time

--- agent/job_endpoint.go
+++ agent/job_endpoint.go
@@ -250,13 +250,11 @@ func (s *HTTPServer) jobAllocations(resp http.ResponseWriter, req *http.Request,
 	if out.Allocations == nil {
 		out.Allocations = make([]*models.AllocListStub, 0)
 	}
-	//defaultTimeZone,_:=time.LoadLocation("UTC")
-	loc,_:=time.LoadLocation("Asia/Shanghai")
 	for  _,aa := range out.Allocations{
 		if !aa.TaskStates[aa.Task].FinishedAt.IsZero(){
-			aa.TaskStates[aa.Task].FinishedAt,_=time.ParseInLocation( "2006-01-02 15:04:05",aa.TaskStates[aa.Task].FinishedAt.Add(8*time.Hour).Format("2006-01-02 15:04:05"),loc)
+			aa.TaskStates[aa.Task].FinishedAt,_=time.ParseInLocation( "2006-01-02 15:04:05",aa.TaskStates[aa.Task].FinishedAt.In(time.Local).Format("2006-01-02 15:04:05"),time.Local)
 		}
-		aa.TaskStates[aa.Task].StartedAt,_=time.ParseInLocation( "2006-01-02 15:04:05",aa.TaskStates[aa.Task].StartedAt.Add(8*time.Hour).Format("2006-01-02 15:04:05"),loc)   //= aa.TaskStates[aa.Task].StartedAt.In(defaultTimeZone).Format("2006-01-02 15:04:05")
+		aa.TaskStates[aa.Task].StartedAt,_=time.ParseInLocation( "2006-01-02 15:04:05",aa.TaskStates[aa.Task].StartedAt.In(time.Local).Format("2006-01-02 15:04:05"),time.Local)
 	}
 	return out.Allocations, nil
 }
