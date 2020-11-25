fccabc0a85a0dbb03f0d826545e3a91eb05f42c2
ffffwh <ffffwh@icloud.com>
2020-11-25 17:41:49 +0800

    update vendor for #547
    Losing gtid on binlog dump thread reconnection.

--- vendor/github.com/siddontang/go-mysql/replication/binlogstreamer.go
+++ vendor/github.com/siddontang/go-mysql/replication/binlogstreamer.go
@@ -86,14 +86,16 @@ func (s *BinlogStreamer) QueueMem() int64 {
 }
 
 func (s *BinlogStreamer) close() {
-	s.closeWithError(ErrSyncClosed)
+	s.closeWithError(nil)
 }
 
 func (s *BinlogStreamer) closeWithError(err error) {
 	if err == nil {
 		err = ErrSyncClosed
+	} else {
+		log.Errorf("close sync with err: %v", err)
 	}
-	log.Errorf("close sync with err: %v", err)
+
 	select {
 	case s.ech <- err:
 	default:
