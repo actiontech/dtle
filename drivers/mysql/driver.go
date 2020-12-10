actiontech/dtle

commit 7406c29a7d00cf0e8dd6948db2e34950fda9f004

move runner initiation to StartTask()
thus correctly returning the error.

drivers/mysql/driver.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/actiontech/dtle/drivers/mysql/common"
+	"github.com/actiontech/dtle/drivers/mysql/kafka"
+	"github.com/actiontech/dtle/drivers/mysql/mysql"
 	"github.com/actiontech/dtle/g"
 	"github.com/armon/go-metrics"
 	"github.com/armon/go-metrics/prometheus"
@@ -472,7 +474,40 @@ func (d *Driver) StartTask(cfg *drivers.TaskConfig) (*drivers.TaskHandle, *drive
 		return nil, nil, errors.Wrap(err, "SetDriverState")
 	}
 	d.tasks.Set(cfg.ID, h)
-	go h.run(&dtleTaskConfig, d)
+
+	{
+		ctx := &common.ExecContext{
+			Subject:    cfg.JobName,
+			StateDir:   d.config.DataDir,
+		}
+		dtleTaskConfig.SetDefaultForEmpty()
+		driverConfig := &common.MySQLDriverConfig{DtleTaskConfig: dtleTaskConfig}
+
+		switch taskTypeFromString(cfg.TaskGroupName) {
+		case taskTypeSrc:
+			h.runner, err = mysql.NewExtractor(ctx, driverConfig, d.logger, d.storeManager, h.waitCh)
+			if err != nil {
+				return nil, nil, errors.Wrap(err, "NewExtractor")
+			}
+		case taskTypeDest:
+			if driverConfig.KafkaConfig != nil {
+				d.logger.Debug("found kafka", "KafkaConfig", driverConfig.KafkaConfig)
+				h.runner = kafka.NewKafkaRunner(ctx, driverConfig.KafkaConfig, d.logger,
+					d.storeManager, d.config.NatsAdvertise, h.waitCh)
+				go h.runner.Run()
+			} else {
+				h.runner, err = mysql.NewApplier(ctx, driverConfig, d.logger, d.storeManager,
+					d.config.NatsAdvertise, h.waitCh,d.eventer, h.taskConfig)
+				if err != nil {
+					return nil, nil, errors.Wrap(err, "NewApplier")
+				}
+			}
+		case taskTypeUnknown:
+			return nil, nil, fmt.Errorf("unknown processor type: %+v", cfg.TaskGroupName)
+		}
+
+	}
+	go h.run(d)
 
 	return handle, nil, nil
 }

drivers/mysql/handle.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/actiontech/dtle/drivers/mysql/common"
-	"github.com/actiontech/dtle/drivers/mysql/kafka"
-	"github.com/actiontech/dtle/drivers/mysql/mysql"
 	"github.com/armon/go-metrics"
 	"github.com/pkg/errors"
 	"strings"
@@ -99,8 +97,7 @@ func (h *taskHandle) IsRunning() bool {
 	return h.procState == drivers.TaskStateRunning
 }
 
-func (h *taskHandle) run(taskConfig *common.DtleTaskConfig, d *Driver) {
-	var err error
+func (h *taskHandle) run(d *Driver) {
 	h.stateLock.Lock()
 	if h.exitResult == nil {
 		h.exitResult = &drivers.ExitResult{}
@@ -108,44 +105,7 @@ func (h *taskHandle) run(taskConfig *common.DtleTaskConfig, d *Driver) {
 	h.procState = drivers.TaskStateRunning
 	h.stateLock.Unlock()
 
-	// TODO: detect if the taskConfig OOMed
-
-	cfg := h.taskConfig
-	ctx := &common.ExecContext{
-		Subject:    cfg.JobName,
-		StateDir:   d.config.DataDir,
-	}
-
-	taskConfig.SetDefaultForEmpty()
-	driverConfig := &common.MySQLDriverConfig{DtleTaskConfig: *taskConfig}
-
-	switch taskTypeFromString(cfg.TaskGroupName) {
-	case taskTypeSrc:
-		h.runner, err = mysql.NewExtractor(ctx, driverConfig, d.logger, d.storeManager, h.waitCh)
-		if err != nil {
-			h.exitResult.Err = errors.Wrap(err, "NewExtractor")
-			return
-		}
-		go h.runner.Run()
-	case taskTypeDest:
-		if taskConfig.KafkaConfig != nil {
-			d.logger.Debug("found kafka", "KafkaConfig", taskConfig.KafkaConfig)
-			h.runner = kafka.NewKafkaRunner(ctx, taskConfig.KafkaConfig, d.logger,
-				d.storeManager, d.config.NatsAdvertise, h.waitCh)
-			go h.runner.Run()
-		} else {
-			h.runner, err = mysql.NewApplier(ctx, driverConfig, d.logger, d.storeManager,
-				d.config.NatsAdvertise, h.waitCh,d.eventer, h.taskConfig)
-			if err != nil {
-				h.exitResult.Err = errors.Wrap(err, "NewApplier")
-				return
-			}
-			go h.runner.Run()
-		}
-	case taskTypeUnknown:
-		h.exitResult.Err = fmt.Errorf("unknown processor type: %+v", cfg.TaskGroupName)
-		return
-	}
+	go h.runner.Run()
 
 	go func() {
 		duration := time.Duration(d.config.StatsCollectionInterval) * time.Second

commit a52fbf8bab5d604ded7dd7112764c98c7de25814

fix repeated kafka run
a bug introduced in 7406c29.

drivers/mysql/driver.go
@@ -495,7 +495,6 @@ func (d *Driver) StartTask(cfg *drivers.TaskConfig) (*drivers.TaskHandle, *drive
 				d.logger.Debug("found kafka", "KafkaConfig", driverConfig.KafkaConfig)
 				h.runner = kafka.NewKafkaRunner(ctx, driverConfig.KafkaConfig, d.logger,
 					d.storeManager, d.config.NatsAdvertise, h.waitCh)
-				go h.runner.Run()
 			} else {
 				h.runner, err = mysql.NewApplier(ctx, driverConfig, d.logger, d.storeManager,
 					d.config.NatsAdvertise, h.waitCh,d.eventer, h.taskConfig)

drivers/mysql/kafka/kafka3.go
@@ -352,6 +352,7 @@ func (kr *KafkaRunner) handleFullCopy() {
 	}
 }
 func (kr *KafkaRunner) handleIncr() {
+	kr.logger.Debug("handleIncr")
 	var err error
 	bigEntries := &common.BinlogEntries{}
 	for !kr.shutdown {
@@ -696,6 +697,7 @@ func (kr *KafkaRunner) kafkaTransformSnapshotData(
 }
 
 func (kr *KafkaRunner) kafkaTransformDMLEventQuery(dmlEvent *common.BinlogEntry) (err error) {
+	kr.logger.Debug("kafkaTransformDMLEventQuery", "gno", dmlEvent.Coordinates.GNO)
 	txSid := dmlEvent.Coordinates.GetSid()
 
 	for i, _ := range dmlEvent.Events {