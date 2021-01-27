actiontech/dtle

commit 79effded67edbfe036f7177158e1c7bab6db9c6f

eliminate race condition when accessing a.gtidSet #574

drivers/mysql/mysql/applier_gtid_executed.go
@@ -245,13 +245,20 @@ func (a *GtidExecutedCreater) createTableGtidExecutedV4() error {
 	}
 }
 
-func (a *ApplierIncr) cleanGtidExecuted(sid uuid.UUID, intervalStr string) error {
+func (a *ApplierIncr) cleanGtidExecuted(sid uuid.UUID, txSid string) error {
 	a.logger.Debug("incr. cleanup before WaitForExecution")
 	if !a.mtsManager.WaitForAllCommitted() {
 		return nil // shutdown
 	}
 	a.logger.Debug("incr. cleanup after WaitForExecution")
 
+	var intervalStr string
+	{
+		a.gtidSetLock.RLock()
+		intervals := base.GetIntervals(a.gtidSet, txSid)
+		intervalStr = base.StringInterval(intervals)
+		a.gtidSetLock.RUnlock()
+	}
 	// The TX is unnecessary if we first insert and then delete.
 	// However, consider `binlog_group_commit_sync_delay > 0`,
 	// `begin; delete; insert; commit;` (1 TX) is faster than `insert; delete;` (2 TX)

drivers/mysql/mysql/applier_incr.go
@@ -15,6 +15,7 @@ import (
 	"github.com/satori/go.uuid"
 	gomysql "github.com/siddontang/go-mysql/mysql"
 	"os"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -43,6 +44,7 @@ type ApplierIncr struct {
 	TotalDeltaCopied  int64
 
 	gtidSet        *gomysql.MysqlGTIDSet
+	gtidSetLock    *sync.RWMutex
 	gtidItemMap    base.GtidItemMap
 	GtidUpdateHook func(*common.BinlogCoordinateTx)
 
@@ -53,7 +55,8 @@ type ApplierIncr struct {
 
 func NewApplierIncr(subject string, mysqlContext *common.MySQLDriverConfig,
 	logger hclog.Logger, gtidSet *gomysql.MysqlGTIDSet, memory2 *int64,
-	db *gosql.DB, dbs []*sql.Conn, shutdownCh chan struct{}) (*ApplierIncr, error) {
+	db *gosql.DB, dbs []*sql.Conn, shutdownCh chan struct{},
+	gtidSetLock *sync.RWMutex) (*ApplierIncr, error) {
 
 	a := &ApplierIncr{
 		logger:                logger,
@@ -67,6 +70,7 @@ func NewApplierIncr(subject string, mysqlContext *common.MySQLDriverConfig,
 		memory2:               memory2,
 		printTps:              os.Getenv(g.ENV_PRINT_TPS) != "",
 		gtidSet:               gtidSet,
+		gtidSetLock:           gtidSetLock,
 		tableItems:            make(mapSchemaTableItems),
 	}
 
@@ -206,11 +210,15 @@ func (a *ApplierIncr) heterogeneousReplay() {
 			txSid := binlogEntry.Coordinates.GetSid()
 
 			gtidSetItem := a.gtidItemMap.GetItem(binlogEntry.Coordinates.SID)
-			intervals := base.GetIntervals(a.gtidSet, txSid)
-			if base.IntervalSlicesContainOne(intervals, binlogEntry.Coordinates.GNO) {
-				// entry executed
-				a.logger.Info("skip an executed tx", "sid", txSid, "gno", binlogEntry.Coordinates.GNO)
-				continue
+			{
+				a.gtidSetLock.RLock()
+				intervals := base.GetIntervals(a.gtidSet, txSid)
+				if base.IntervalSlicesContainOne(intervals, binlogEntry.Coordinates.GNO) {
+					// entry executed
+					a.logger.Info("skip an executed tx", "sid", txSid, "gno", binlogEntry.Coordinates.GNO)
+					continue
+				}
+				a.gtidSetLock.RUnlock()
 			}
 			// endregion
 			// this must be after duplication check
@@ -224,7 +232,7 @@ func (a *ApplierIncr) heterogeneousReplay() {
 
 			a.logger.Debug("gtidSetItem", "NRow", gtidSetItem.NRow)
 			if gtidSetItem.NRow >= cleanupGtidExecutedLimit {
-				err = a.cleanGtidExecuted(binlogEntry.Coordinates.SID, base.StringInterval(intervals))
+				err = a.cleanGtidExecuted(binlogEntry.Coordinates.SID, txSid)
 				if err != nil {
 					a.OnError(TaskStateDead, err)
 					return

drivers/mysql/kafka/kafka3.go
@@ -112,10 +112,6 @@ func NewKafkaRunner(execCtx *common.ExecContext, cfg *common.KafkaConfig, logger
 	return kr
 }
 
-func (kr *KafkaRunner) updateGtidString() {
-	kr.Gtid = kr.gtidSet.String()
-	kr.logger.Debug("kafka. updateGtidString", "gtid", kr.Gtid)
-}
 func (kr *KafkaRunner) updateGtidLoop() {
 	updateGtidInterval := 15 * time.Second
 	for !kr.shutdown {
@@ -1056,7 +1052,8 @@ func (kr *KafkaRunner) kafkaTransformDMLEventQueries(dmlEntries []*common.Binlog
 	kr.BinlogPos = curDmlEntry.Coordinates.LogPos
 
 	common.UpdateGtidSet(kr.gtidSet, curDmlEntry.Coordinates.SID, curDmlEntry.Coordinates.GNO)
-	kr.updateGtidString()
+	kr.Gtid = kr.gtidSet.String()
+	kr.logger.Debug("kafka. updateGtidString", "gtid", kr.Gtid)
 
 	kr.logger.Debug("kafka: after kafkaTransformDMLEventQueries")
 	return nil

drivers/mysql/mysql/applier.go
@@ -84,6 +84,7 @@ type Applier struct {
 	stubFullApplyDelay time.Duration
 
 	gtidSet *gomysql.MysqlGTIDSet
+	gtidSetLock *sync.RWMutex
 
 	storeManager *common.StoreManager
 	gtidCh       chan *common.BinlogCoordinateTx
@@ -109,6 +110,7 @@ func NewApplier(
 		rowCopyComplete: make(chan struct{}),
 		copyRowsQueue:   make(chan *common.DumpEntry, 24),
 		waitCh:          waitCh,
+		gtidSetLock:     &sync.RWMutex{},
 		shutdownCh:      make(chan struct{}),
 		storeManager:    storeManager,
 		gtidCh:          make(chan *common.BinlogCoordinateTx, 4096),
@@ -191,7 +193,9 @@ func (a *Applier) updateGtidLoop() {
 				doUpdate()
 				doUpload()
 			} else {
+				a.gtidSetLock.Lock()
 				common.UpdateGtidSet(a.gtidSet, coord.SID, coord.GNO)
+				a.gtidSetLock.Unlock()
 				file = coord.LogFile
 				pos = coord.LogPos
 			}
@@ -241,7 +245,7 @@ func (a *Applier) Run() {
 	}
 
 	a.ai, err = NewApplierIncr(a.subject, a.mysqlContext, a.logger, a.gtidSet, a.memory2,
-		a.db, a.dbs, a.shutdownCh)
+		a.db, a.dbs, a.shutdownCh, a.gtidSetLock)
 	if err != nil {
 		a.onError(TaskStateDead, errors.Wrap(err, "NewApplierIncr"))
 		return

commit ba5a849444e59b4448963e61682b13347bace614

fix skipped mutex unlock
a bug introduced in 79effded.

drivers/mysql/mysql/applier_gtid_executed.go
@@ -252,13 +252,13 @@ func (a *ApplierIncr) cleanGtidExecuted(sid uuid.UUID, txSid string) error {
 	}
 	a.logger.Debug("incr. cleanup after WaitForExecution")
 
-	var intervalStr string
-	{
+	intervalStr := func() string {
 		a.gtidSetLock.RLock()
+		defer a.gtidSetLock.RUnlock()
 		intervals := base.GetIntervals(a.gtidSet, txSid)
-		intervalStr = base.StringInterval(intervals)
-		a.gtidSetLock.RUnlock()
-	}
+		return base.StringInterval(intervals)
+	}()
+
 	// The TX is unnecessary if we first insert and then delete.
 	// However, consider `binlog_group_commit_sync_delay > 0`,
 	// `begin; delete; insert; commit;` (1 TX) is faster than `insert; delete;` (2 TX)

drivers/mysql/mysql/applier_incr.go
@@ -210,15 +210,15 @@ func (a *ApplierIncr) heterogeneousReplay() {
 			txSid := binlogEntry.Coordinates.GetSid()
 
 			gtidSetItem := a.gtidItemMap.GetItem(binlogEntry.Coordinates.SID)
-			{
+			txExecuted := func() bool {
 				a.gtidSetLock.RLock()
+				defer a.gtidSetLock.RUnlock()
 				intervals := base.GetIntervals(a.gtidSet, txSid)
-				if base.IntervalSlicesContainOne(intervals, binlogEntry.Coordinates.GNO) {
-					// entry executed
-					a.logger.Info("skip an executed tx", "sid", txSid, "gno", binlogEntry.Coordinates.GNO)
-					continue
-				}
-				a.gtidSetLock.RUnlock()
+				return base.IntervalSlicesContainOne(intervals, binlogEntry.Coordinates.GNO)
+			}()
+			if txExecuted {
+				a.logger.Info("skip an executed tx", "sid", txSid, "gno", binlogEntry.Coordinates.GNO)
+				continue
 			}
 			// endregion
 			// this must be after duplication check