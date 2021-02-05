3b1061fe620927b9c2660acfe22fa03575a6ebdd
luowei <luowei@actiontech.sky>
2021-02-05 17:48:27 +0800

    delete dml may get job failed

--- drivers/mysql/kafka/kafka3.go
+++ drivers/mysql/kafka/kafka3.go
@@ -841,8 +841,6 @@ func (kr *KafkaRunner) kafkaTransformDMLEventQueries(dmlEntries []*common.Binlog
 				after = NewRow()
 			}
 
-			tableIdents = append(tableIdents, fmt.Sprintf("%v.%v.%v", kr.kafkaMgr.Cfg.Topic, table.TableSchema, table.TableName))
-
 			keyPayload := NewRow()
 			colList := table.OriginalTableColumns.ColumnList()
 
@@ -1041,6 +1039,8 @@ func (kr *KafkaRunner) kafkaTransformDMLEventQueries(dmlEntries []*common.Binlog
 				return err
 			}
 
+			tableIdent := fmt.Sprintf("%v.%v.%v", kr.kafkaMgr.Cfg.Topic, table.TableSchema, table.TableName)
+			tableIdents = append(tableIdents, tableIdent)
 			keysBs = append(keysBs, kBs)
 			valuesBs = append(valuesBs, vBs)
 			vBs = []byte(strings.Replace(string(vBs), "\"field\":\"snapshot\"", "\"default\":false,\"field\":\"snapshot\"", -1))
@@ -1056,6 +1056,7 @@ func (kr *KafkaRunner) kafkaTransformDMLEventQueries(dmlEntries []*common.Binlog
 					return err
 				}
 
+				tableIdents = append(tableIdents, tableIdent)
 				keysBs = append(keysBs, kBs)
 				valuesBs = append(valuesBs, v2Bs)
 			}
