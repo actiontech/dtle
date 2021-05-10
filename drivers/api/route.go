5a9c5ddde772190e75c90187b777eed9cd44bb18
luowei <luowei@actiontech.sky>
2021-05-10 13:21:57 +0800

    change api name to make it more precise #645

--- drivers/api/route.go
+++ drivers/api/route.go
@@ -59,7 +59,7 @@ func SetupApiServer(logger hclog.Logger, apiAddr, nomadAddr, uiDir string) (err 
 	e.POST("/v2/job/migration", v2.CreateOrUpdateMigrationJobV2)
 	e.GET("/v2/nodes", v2.NodeListV2)
 	e.POST("/v2/validation/job", v2.ValidateJobV2)
-	e.GET("/v2/database/schemas", v2.ListDatabaseSchemasV2)
+	e.GET("/v2/mysql/schemas", v2.ListMysqlSchemasV2)
 	e.GET("/v2/monitor/task", v2.GetTaskProgressV2)
 
 	e.Validator = handler.NewValidator()
