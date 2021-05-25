83354db3e0f212fe2346df7c9a0fb928828f08d1
yangzhongjiao <451773851@qq.com>
2021-05-25 17:51:38 +0800

    modify test case

--- drivers/mysql/mysql/base/binlog_test.go
+++ drivers/mysql/mysql/base/binlog_test.go
@@ -13,33 +13,41 @@ import (
 )
 
 func TestBinlogCoordinates(t *testing.T) {
-	c1 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 104}
-	c2 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 104}
-	c3 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 5000}
-	c4 := BinlogCoordinateTx{LogFile: "mysql-bin.00112", LogPos: 104}
+	c1 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 104}
+	c2 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 104}
+	c3 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 5000}
+	c4 := BinlogCoordinatesX{LogFile: "mysql-bin.00099", LogPos: 104}
+	c5 := BinlogCoordinatesX{LogFile: "mysql-bin.00012", LogPos: 5000}
+	c6 := BinlogCoordinatesX{LogFile: "mysql-bin.00012", LogPos: 104}
 
-	test.S(t).ExpectTrue(c1.Equals(&c2))
-	test.S(t).ExpectFalse(c1.Equals(&c3))
-	test.S(t).ExpectFalse(c1.Equals(&c4))
-	test.S(t).ExpectFalse(c1.SmallerThan(&c2))
-	test.S(t).ExpectTrue(c1.SmallerThan(&c3))
-	test.S(t).ExpectTrue(c1.SmallerThan(&c4))
-	test.S(t).ExpectTrue(c3.SmallerThan(&c4))
-	test.S(t).ExpectFalse(c3.SmallerThan(&c2))
-	test.S(t).ExpectFalse(c4.SmallerThan(&c2))
-	test.S(t).ExpectFalse(c4.SmallerThan(&c3))
+	// equal
 
-	test.S(t).ExpectTrue(c1.SmallerThanOrEquals(&c2))
-	test.S(t).ExpectTrue(c1.SmallerThanOrEquals(&c3))
+	test.S(t).ExpectEquals(c1.CompareFilePos(&c2), 0)    // file = && pos =
+	test.S(t).ExpectNotEquals(c1.CompareFilePos(&c3), 0) // file = && pos !=
+	test.S(t).ExpectNotEquals(c1.CompareFilePos(&c4), 0) // file != && pos =
+	test.S(t).ExpectNotEquals(c3.CompareFilePos(&c4), 0) // file != && pos !=
+
+	// smaller
+
+	test.S(t).ExpectEquals(c1.CompareFilePos(&c3), -1) // file = && pos <
+	test.S(t).ExpectEquals(c6.CompareFilePos(&c3), -1) // file < && pos <
+	test.S(t).ExpectEquals(c5.CompareFilePos(&c3), -1) // file < && pos =
+	test.S(t).ExpectEquals(c5.CompareFilePos(&c1), -1) // file < && pos >
+
+	//// bigger
+	test.S(t).ExpectEquals(c3.CompareFilePos(&c1), 1) // file = && pos >
+	test.S(t).ExpectEquals(c4.CompareFilePos(&c3), 1) // file > && pos <
+	test.S(t).ExpectEquals(c4.CompareFilePos(&c1), 1) // file > && pos =
+	test.S(t).ExpectEquals(c3.CompareFilePos(&c6), 1) // file > && pos >
 }
 
 func TestBinlogCoordinatesAsKey(t *testing.T) {
-	m := make(map[BinlogCoordinateTx]bool)
+	m := make(map[BinlogCoordinatesX]bool)
 
-	c1 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 104}
-	c2 := BinlogCoordinateTx{LogFile: "mysql-bin.00022", LogPos: 104}
-	c3 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 104}
-	c4 := BinlogCoordinateTx{LogFile: "mysql-bin.00017", LogPos: 222}
+	c1 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 104}
+	c2 := BinlogCoordinatesX{LogFile: "mysql-bin.00022", LogPos: 104}
+	c3 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 104}
+	c4 := BinlogCoordinatesX{LogFile: "mysql-bin.00017", LogPos: 222}
 
 	m[c1] = true
 	m[c2] = true
@@ -49,51 +57,25 @@ func TestBinlogCoordinatesAsKey(t *testing.T) {
 	test.S(t).ExpectEquals(len(m), 3)
 }
 
-func TestBinlogCoordinates_DisplayString(t *testing.T) {
-	type fields struct {
-		LogFile string
-		LogPos  int64
-		GtidSet string
-		Type    BinlogType
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &BinlogCoordinateTx{
-				LogFile: tt.fields.LogFile,
-				LogPos:  tt.fields.LogPos,
-				GtidSet: tt.fields.GtidSet,
-			}
-			if got := b.DisplayString(); got != tt.want {
-				t.Errorf("BinlogCoordinates.DisplayString() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestBinlogCoordinates_String(t *testing.T) {
 	type fields struct {
 		LogFile string
 		LogPos  int64
 		GtidSet string
-		Type    BinlogType
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   string
 	}{
-	// TODO: Add test cases.
+		{"emptyLog", fields{}, ""},
+		{"emptyGtid", fields{LogFile: "mysql-bin.00017", LogPos: 104}, ""},
+		{"gtid1", fields{GtidSet: "server_id:121"}, "server_id:121"},
+		{"gtid2", fields{GtidSet: "108cc4a4-0d40-11ea-9598-2016d8c96b66:1-5,\nc42216ad-0d37-11ea-b163-2016d8c96b56:1-9,\nceabbacf-0c77-11ea-b49f-2016d8c96b46:1-1662590"}, "108cc4a4-0d40-11ea-9598-2016d8c96b66:1-5,\nc42216ad-0d37-11ea-b163-2016d8c96b56:1-9,\nceabbacf-0c77-11ea-b49f-2016d8c96b46:1-1662590"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := BinlogCoordinateTx{
+			b := BinlogCoordinatesX{
 				LogFile: tt.fields.LogFile,
 				LogPos:  tt.fields.LogPos,
 				GtidSet: tt.fields.GtidSet,
@@ -105,125 +87,32 @@ func TestBinlogCoordinates_String(t *testing.T) {
 	}
 }
 
-func TestBinlogCoordinates_Equals(t *testing.T) {
-	type fields struct {
-		LogFile string
-		LogPos  int64
-		GtidSet string
-		Type    BinlogType
-	}
-	type args struct {
-		other *BinlogCoordinateTx
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &BinlogCoordinateTx{
-				LogFile: tt.fields.LogFile,
-				LogPos:  tt.fields.LogPos,
-				GtidSet: tt.fields.GtidSet,
-			}
-			if got := b.Equals(tt.args.other); got != tt.want {
-				t.Errorf("BinlogCoordinates.Equals() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestBinlogCoordinates_IsEmpty(t *testing.T) {
 	type fields struct {
 		LogFile string
 		LogPos  int64
 		GtidSet string
-		Type    BinlogType
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		want   bool
 	}{
-	// TODO: Add test cases.
+		{"emptyTrue", fields{}, true},
+		{"allEmpty", fields{LogFile: "", LogPos: 0}, true},
+		{"gtidEmpty", fields{LogFile: "mysql-bin.00017", LogPos: 104}, false},
+		{"logFileEmpty", fields{GtidSet: "server_id:121"}, false},
+		{"gtidEmpty", fields{LogFile: "mysql-bin.00017"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := &BinlogCoordinateTx{
+			b := &BinlogCoordinatesX{
 				LogFile: tt.fields.LogFile,
 				LogPos:  tt.fields.LogPos,
 				GtidSet: tt.fields.GtidSet,
 			}
 			if got := b.IsEmpty(); got != tt.want {
 				t.Errorf("BinlogCoordinates.IsEmpty() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestBinlogCoordinates_SmallerThan(t *testing.T) {
-	type fields struct {
-		LogFile string
-		LogPos  int64
-		GtidSet string
-		Type    BinlogType
-	}
-	type args struct {
-		other *BinlogCoordinateTx
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &BinlogCoordinateTx{
-				LogFile: tt.fields.LogFile,
-				LogPos:  tt.fields.LogPos,
-				GtidSet: tt.fields.GtidSet,
-			}
-			if got := b.SmallerThan(tt.args.other); got != tt.want {
-				t.Errorf("BinlogCoordinates.SmallerThan() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestBinlogCoordinates_SmallerThanOrEquals(t *testing.T) {
-	type fields struct {
-		LogFile string
-		LogPos  int64
-		GtidSet string
-		Type    BinlogType
-	}
-	type args struct {
-		other *BinlogCoordinateTx
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b := &BinlogCoordinateTx{
-				LogFile: tt.fields.LogFile,
-				LogPos:  tt.fields.LogPos,
-				GtidSet: tt.fields.GtidSet,
-			}
-			if got := b.SmallerThanOrEquals(tt.args.other); got != tt.want {
-				t.Errorf("BinlogCoordinates.SmallerThanOrEquals() = %v, want %v", got, tt.want)
 			}
 		})
 	}
