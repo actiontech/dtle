d52a4a7a108116eca54bc645f44617db4c366452
luowei <luowei@actiontech.sky>
2021-06-04 13:34:09 +0800

    restart dtle after upgrade to make it perform like post-install

--- misc/pre-upgrade.sh
+++ misc/pre-upgrade.sh
@@ -4,6 +4,4 @@ which systemctl &>/dev/null
 if [[ $? -eq 0 ]]; then
     systemctl stop dtle-consul
     systemctl stop dtle-nomad
-else
-    echo "No systemd. Please stop dtle-nomad manually before upgrade."
 fi
