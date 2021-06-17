88995c5032685bcfc7598bc1c048071f954bd71b
ffffwh <ffffwh@icloud.com>
2021-06-17 17:37:17 +0800

    check whether runner is nil

--- drivers/mysql/handle.go
+++ drivers/mysql/handle.go
@@ -54,11 +54,14 @@ func (h *taskHandle) TaskStatus() (*drivers.TaskStatus, error) {
 
 	m := map[string]string{}
 
-	stat, err := h.runner.Stats()
-	if err != nil {
-		return nil, errors.Wrap(err, "runner.Stats")
+	if h.runner != nil {
+		stat, err := h.runner.Stats()
+		if err != nil {
+			return nil, errors.Wrap(err, "runner.Stats")
+		}
+		m["GtidSet"] = stat.CurrentCoordinates.GtidSet
 	}
-	m["GtidSet"] = stat.CurrentCoordinates.GtidSet
+
 	// TODO Cannot get InspectTask -> TaskStatus called by any API.
 	// See https://github.com/hashicorp/nomad/issues/4848
 	return &drivers.TaskStatus{
