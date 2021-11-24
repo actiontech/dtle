actiontech/dtle

commit 6e95ce148d8857ffa3651ffbcd4be1dcc683e6e3

refactor around replicateDoDb and move GetCandidateUniqueKeys()

drivers/mysql/mysql/extractor.go
@@ -400,9 +400,8 @@ func (e *Extractor) inspectTables() (err error) {
 		var doDbs []*common.DataSource
 		// Get all db from TableSchemaRegex regex and get all tableSchemaRename
 		for _, doDb := range e.mysqlContext.ReplicateDoDb {
-			var regex string
 			if doDb.TableSchemaRegex != "" && doDb.TableSchemaRename != "" {
-				regex = doDb.TableSchemaRegex
+				regex := doDb.TableSchemaRegex
 				schemaRenameRegex := doDb.TableSchemaRename
 				for _, db := range dbsFiltered {
 					newdb := &common.DataSource{}
@@ -433,11 +432,8 @@ func (e *Extractor) inspectTables() (err error) {
 			}
 		}
 		for _, doDb := range doDbs {
-			db := &common.DataSource{
-				TableSchema:       doDb.TableSchema,
-				TableSchemaRegex:  doDb.TableSchemaRegex,
-				TableSchemaRename: doDb.TableSchemaRename,
-			}
+			schemaCtx := common.NewSchemaContext(doDb.TableSchema)
+			e.replicateDoDb[doDb.TableSchema] = schemaCtx
 
 			existedTables, err := sql.ShowTables(e.db, doDb.TableSchema, e.mysqlContext.ExpandSyntaxSupport)
 			if err != nil {
@@ -445,7 +441,8 @@ func (e *Extractor) inspectTables() (err error) {
 			}
 			tbsFiltered := []*common.Table{}
 			for _, tb := range existedTables {
-				if len(e.mysqlContext.ReplicateIgnoreDb) > 0 && common.IgnoreTbByReplicateIgnoreDb(e.mysqlContext.ReplicateIgnoreDb, db.TableSchema, tb.TableName) {
+				if len(e.mysqlContext.ReplicateIgnoreDb) > 0 &&
+					common.IgnoreTbByReplicateIgnoreDb(e.mysqlContext.ReplicateIgnoreDb, doDb.TableSchema, tb.TableName) {
 					continue
 				}
 				tbsFiltered = append(tbsFiltered, tb)
@@ -460,7 +457,10 @@ func (e *Extractor) inspectTables() (err error) {
 							"schema", doDb.TableSchema, "table", doTb.TableName)
 						continue
 					}
-					db.Tables = append(db.Tables, doTb)
+					err = schemaCtx.AddTable(doTb)
+					if err != nil {
+						return err
+					}
 				}
 			} else { // replicate selected tables
 				for _, doTb := range doDb.Tables {
@@ -488,7 +488,10 @@ func (e *Extractor) inspectTables() (err error) {
 								e.logger.Warn("ValidateOriginalTable error", "TableSchema", doDb.TableSchema, "TableName", doTb.TableName, "err", err)
 								continue
 							}
-							db.Tables = append(db.Tables, newTable)
+							err = schemaCtx.AddTable(newTable)
+							if err != nil {
+								return err
+							}
 						}
 						//if db.Tables == nil {
 						//	return fmt.Errorf("src table was nil")
@@ -505,22 +508,17 @@ func (e *Extractor) inspectTables() (err error) {
 							}
 							newTable := &common.Table{}
 							*newTable = *doTb
-							db.Tables = append(db.Tables, newTable)
+							err = schemaCtx.AddTable(newTable)
+							if err != nil {
+								return err
+							}
 						}
 					} else {
 						return fmt.Errorf("table configuration error")
 					}
-
 				}
 			}
-			schemaCtx := common.NewSchemaContext(db.TableSchema)
-			err = schemaCtx.AddTables(db.Tables)
-			if err != nil {
-				return err
-			}
-			e.replicateDoDb[db.TableSchema] = schemaCtx
 		}
-		//	e.mysqlContext.ReplicateDoDb = e.replicateDoDb
 	} else { // empty DoDB. replicate all db/tb
 		for _, dbName := range dbsFiltered {
 			ds := &common.DataSource{
@@ -551,26 +549,6 @@ func (e *Extractor) inspectTables() (err error) {
 			e.replicateDoDb[ds.TableSchema] = schemaCtx
 		}
 	}
-	/*if e.mysqlContext.ExpandSyntaxSupport {
-		db_mysql := &config.DataSource{
-			TableSchema: "mysql",
-		}
-		db_mysql.Tables = append(db_mysql.Tables,
-			&config.Table{
-				TableSchema: "mysql",
-				TableName:   "user",
-			},
-			&config.Table{
-				TableSchema: "mysql",
-				TableName:   "proc",
-			},
-			&config.Table{
-				TableSchema: "mysql",
-				TableName:   "func",
-			},
-		)
-		e.replicateDoDb = append(e.replicateDoDb, db_mysql)
-	}*/
 
 	return nil
 }

drivers/mysql/common/tabletypes.go
@@ -22,6 +22,13 @@ func NewSchemaContext(name string) *SchemaContext {
 		TableMap: map[string]*TableContext{},
 	}
 }
+func (sc *SchemaContext) AddTable(table *Table) (err error) {
+	sc.TableMap[table.TableName], err = NewTableContext(table)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 func (sc *SchemaContext) AddTables(tables []*Table) (err error) {
 	for _, table := range tables {
 		sc.TableMap[table.TableName], err = NewTableContext(table)

drivers/mysql/mysql/base/utils.go
@@ -529,3 +529,97 @@ func GetTableColumnsSqle(sqleContext *sqle.Context, schema string,
 	//r.SetCharset() // TODO
 	return r, fkParents, nil
 }
+
+func GetCandidateUniqueKeys(logger g.LoggerType, db usql.QueryAble, databaseName, tableName string) (uniqueKeys [](*common.UniqueKey), err error) {
+	query := `
+SELECT UNIQUES.INDEX_NAME, UNIQUES.COLUMN_NAMES, LOCATE('auto_increment', EXTRA) > 0 as is_auto_increment, has_nullable
+FROM INFORMATION_SCHEMA.COLUMNS
+     INNER JOIN
+     (SELECT TABLE_SCHEMA, TABLE_NAME, INDEX_NAME,
+             GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
+             SUBSTRING_INDEX(GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC), ',', 1) AS FIRST_COLUMN_NAME,
+             SUM(NULLABLE='YES') > 0 AS has_nullable
+      FROM INFORMATION_SCHEMA.STATISTICS
+      WHERE NON_UNIQUE=0 AND TABLE_SCHEMA = ? AND TABLE_NAME = ?
+      GROUP BY TABLE_SCHEMA,TABLE_NAME,INDEX_NAME) AS UNIQUES
+     ON (COLUMNS.TABLE_SCHEMA = UNIQUES.TABLE_SCHEMA
+         AND COLUMNS.TABLE_NAME = UNIQUES.TABLE_NAME
+         AND COLUMNS.COLUMN_NAME = UNIQUES.FIRST_COLUMN_NAME)
+WHERE COLUMNS.TABLE_SCHEMA = ? AND COLUMNS.TABLE_NAME = ?`
+	/*query := `
+	    SELECT
+	      COLUMNS.TABLE_SCHEMA,
+	      COLUMNS.TABLE_NAME,
+	      COLUMNS.COLUMN_NAME,
+	      UNIQUES.INDEX_NAME,
+	      UNIQUES.COLUMN_NAMES,
+	      UNIQUES.COUNT_COLUMN_IN_INDEX,
+	      COLUMNS.DATA_TYPE,
+	      COLUMNS.CHARACTER_SET_NAME,
+				LOCATE('auto_increment', EXTRA) > 0 as is_auto_increment,
+	      has_nullable
+	    FROM INFORMATION_SCHEMA.COLUMNS INNER JOIN (
+	      SELECT
+	        TABLE_SCHEMA,
+	        TABLE_NAME,
+	        INDEX_NAME,
+	        COUNT(*) AS COUNT_COLUMN_IN_INDEX,
+	        GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
+	        SUBSTRING_INDEX(GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC), ',', 1) AS FIRST_COLUMN_NAME,
+	        SUM(NULLABLE='YES') > 0 AS has_nullable
+	      FROM INFORMATION_SCHEMA.STATISTICS
+	      WHERE
+					NON_UNIQUE=0
+					AND TABLE_SCHEMA = ?
+	      	AND TABLE_NAME = ?
+	      GROUP BY TABLE_SCHEMA, TABLE_NAME, INDEX_NAME
+	    ) AS UNIQUES
+	    ON (
+	      COLUMNS.TABLE_SCHEMA = UNIQUES.TABLE_SCHEMA AND
+	      COLUMNS.TABLE_NAME = UNIQUES.TABLE_NAME AND
+	      COLUMNS.COLUMN_NAME = UNIQUES.FIRST_COLUMN_NAME
+	    )
+	    WHERE
+	      COLUMNS.TABLE_SCHEMA = ?
+	      AND COLUMNS.TABLE_NAME = ?
+	    ORDER BY
+	      COLUMNS.TABLE_SCHEMA, COLUMNS.TABLE_NAME,
+	      CASE UNIQUES.INDEX_NAME
+	        WHEN 'PRIMARY' THEN 0
+	        ELSE 1
+	      END,
+	      CASE has_nullable
+	        WHEN 0 THEN 0
+	        ELSE 1
+	      END,
+	      CASE IFNULL(CHARACTER_SET_NAME, '')
+	          WHEN '' THEN 0
+	          ELSE 1
+	      END,
+	      CASE DATA_TYPE
+	        WHEN 'tinyint' THEN 0
+	        WHEN 'smallint' THEN 1
+	        WHEN 'int' THEN 2
+	        WHEN 'bigint' THEN 3
+	        ELSE 100
+	      END,
+	      COUNT_COLUMN_IN_INDEX
+	  `*/
+	err = usql.QueryRowsMap(db, query, func(m usql.RowMap) error {
+		columns := common.ParseColumnList(m.GetString("COLUMN_NAMES"))
+		uniqueKey := &common.UniqueKey{
+			Name:            m.GetString("INDEX_NAME"),
+			Columns:         *columns,
+			HasNullable:     m.GetBool("has_nullable"),
+			IsAutoIncrement: m.GetBool("is_auto_increment"),
+			LastMaxVals:     make([]string, len(columns.Columns)),
+		}
+		uniqueKeys = append(uniqueKeys, uniqueKey)
+		return nil
+	}, databaseName, tableName, databaseName, tableName)
+	if err != nil {
+		return uniqueKeys, err
+	}
+	logger.Debug("Potential unique keys.", "schema", databaseName, "table", tableName, "uniqueKeys", uniqueKeys)
+	return uniqueKeys, nil
+}

drivers/mysql/mysql/inspect.go
@@ -174,8 +174,10 @@ func (i *Inspector) ValidateOriginalTable(databaseName, tableName string, table 
 	return nil
 }
 
-func (i *Inspector) InspectTableColumnsAndUniqueKeys(databaseName, tableName string) (columns *common.ColumnList, uniqueKeys [](*common.UniqueKey), err error) {
-	uniqueKeys, err = i.getCandidateUniqueKeys(databaseName, tableName)
+func (i *Inspector) InspectTableColumnsAndUniqueKeys(databaseName, tableName string) (
+	columns *common.ColumnList, uniqueKeys []*common.UniqueKey, err error) {
+
+	uniqueKeys, err = ubase.GetCandidateUniqueKeys(i.logger, i.db, databaseName, tableName)
 	if err != nil {
 		return columns, uniqueKeys, err
 	}
@@ -367,102 +369,4 @@ func (i *Inspector) validateTableTriggers(databaseName, tableName string) error 
 		return fmt.Errorf("Found triggers on %s.%s. Triggers are not supported at this time. Bailing out", umconf.EscapeName(databaseName), umconf.EscapeName(tableName))
 	}
 	return nil
-}
-
-// getCandidateUniqueKeys investigates a table and returns the list of unique keys
-// candidate for chunking
-func (i *Inspector) getCandidateUniqueKeys(databaseName, tableName string) (uniqueKeys [](*common.UniqueKey), err error) {
-	query := `SELECT
-      UNIQUES.INDEX_NAME,UNIQUES.COLUMN_NAMES,LOCATE('auto_increment', EXTRA) > 0 as is_auto_increment,has_nullable
-    FROM INFORMATION_SCHEMA.COLUMNS INNER JOIN (
-      SELECT
-        TABLE_SCHEMA,TABLE_NAME,INDEX_NAME,GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
-        SUBSTRING_INDEX(GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC), ',', 1) AS FIRST_COLUMN_NAME,
-        SUM(NULLABLE='YES') > 0 AS has_nullable
-      FROM INFORMATION_SCHEMA.STATISTICS
-      WHERE
-			NON_UNIQUE=0 AND TABLE_SCHEMA = ? AND TABLE_NAME = ?
-      GROUP BY TABLE_SCHEMA,TABLE_NAME,INDEX_NAME
-    ) AS UNIQUES
-    ON (
-      COLUMNS.TABLE_SCHEMA = UNIQUES.TABLE_SCHEMA AND COLUMNS.TABLE_NAME = UNIQUES.TABLE_NAME AND COLUMNS.COLUMN_NAME = UNIQUES.FIRST_COLUMN_NAME
-    )
-    WHERE
-      COLUMNS.TABLE_SCHEMA = ? AND COLUMNS.TABLE_NAME = ?`
-	/*query := `
-	    SELECT
-	      COLUMNS.TABLE_SCHEMA,
-	      COLUMNS.TABLE_NAME,
-	      COLUMNS.COLUMN_NAME,
-	      UNIQUES.INDEX_NAME,
-	      UNIQUES.COLUMN_NAMES,
-	      UNIQUES.COUNT_COLUMN_IN_INDEX,
-	      COLUMNS.DATA_TYPE,
-	      COLUMNS.CHARACTER_SET_NAME,
-				LOCATE('auto_increment', EXTRA) > 0 as is_auto_increment,
-	      has_nullable
-	    FROM INFORMATION_SCHEMA.COLUMNS INNER JOIN (
-	      SELECT
-	        TABLE_SCHEMA,
-	        TABLE_NAME,
-	        INDEX_NAME,
-	        COUNT(*) AS COUNT_COLUMN_IN_INDEX,
-	        GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
-	        SUBSTRING_INDEX(GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC), ',', 1) AS FIRST_COLUMN_NAME,
-	        SUM(NULLABLE='YES') > 0 AS has_nullable
-	      FROM INFORMATION_SCHEMA.STATISTICS
-	      WHERE
-					NON_UNIQUE=0
-					AND TABLE_SCHEMA = ?
-	      	AND TABLE_NAME = ?
-	      GROUP BY TABLE_SCHEMA, TABLE_NAME, INDEX_NAME
-	    ) AS UNIQUES
-	    ON (
-	      COLUMNS.TABLE_SCHEMA = UNIQUES.TABLE_SCHEMA AND
-	      COLUMNS.TABLE_NAME = UNIQUES.TABLE_NAME AND
-	      COLUMNS.COLUMN_NAME = UNIQUES.FIRST_COLUMN_NAME
-	    )
-	    WHERE
-	      COLUMNS.TABLE_SCHEMA = ?
-	      AND COLUMNS.TABLE_NAME = ?
-	    ORDER BY
-	      COLUMNS.TABLE_SCHEMA, COLUMNS.TABLE_NAME,
-	      CASE UNIQUES.INDEX_NAME
-	        WHEN 'PRIMARY' THEN 0
-	        ELSE 1
-	      END,
-	      CASE has_nullable
-	        WHEN 0 THEN 0
-	        ELSE 1
-	      END,
-	      CASE IFNULL(CHARACTER_SET_NAME, '')
-	          WHEN '' THEN 0
-	          ELSE 1
-	      END,
-	      CASE DATA_TYPE
-	        WHEN 'tinyint' THEN 0
-	        WHEN 'smallint' THEN 1
-	        WHEN 'int' THEN 2
-	        WHEN 'bigint' THEN 3
-	        ELSE 100
-	      END,
-	      COUNT_COLUMN_IN_INDEX
-	  `*/
-	err = usql.QueryRowsMap(i.db, query, func(m usql.RowMap) error {
-		columns := common.ParseColumnList(m.GetString("COLUMN_NAMES"))
-		uniqueKey := &common.UniqueKey{
-			Name:            m.GetString("INDEX_NAME"),
-			Columns:         *columns,
-			HasNullable:     m.GetBool("has_nullable"),
-			IsAutoIncrement: m.GetBool("is_auto_increment"),
-			LastMaxVals:     make([]string, len(columns.Columns)),
-		}
-		uniqueKeys = append(uniqueKeys, uniqueKey)
-		return nil
-	}, databaseName, tableName, databaseName, tableName)
-	if err != nil {
-		return uniqueKeys, err
-	}
-	i.logger.Debug("Potential unique keys.", "schema", databaseName, "table", tableName, "uniqueKeys", uniqueKeys)
-	return uniqueKeys, nil
 }

commit c58098ab7a72ab71516c21036f52fc7c51774468

fix SchemaRename not taking effect
a bug introduced in 6e95ce14.

misc: logs.

drivers/mysql/mysql/binlog/binlog_reader.go
@@ -753,13 +753,15 @@ func (b *BinlogReader) setDtleQuery(query string) string {
 }
 
 func (b *BinlogReader) sendEntry(entriesChannel chan<- *common.BinlogEntryContext) {
-	if b.entryContext.Entry.IsPartOfBigTx() {
+	isBig := b.entryContext.Entry.IsPartOfBigTx()
+	if isBig {
 		newVal := atomic.AddInt32(&b.BigTxCount, 1)
 		if newVal == 1 {
 			g.AddBigTxJob()
 		}
 	}
-	b.logger.Debug("sendEntry", "gno", b.entryContext.Entry.Coordinates.GNO, "events", len(b.entryContext.Entry.Events))
+	b.logger.Debug("sendEntry", "gno", b.entryContext.Entry.Coordinates.GNO, "events", len(b.entryContext.Entry.Events),
+		"isBig", isBig)
 	atomic.AddInt64(b.memory, int64(b.entryContext.Entry.Size()))
 	entriesChannel <- b.entryContext
 	atomic.AddUint32(&b.extractedTxCount, 1)

drivers/mysql/mysql/extractor.go
@@ -434,6 +434,7 @@ func (e *Extractor) inspectTables() (err error) {
 		}
 		for _, doDb := range doDbs {
 			schemaCtx := common.NewSchemaContext(doDb.TableSchema)
+			schemaCtx.TableSchemaRename = doDb.TableSchemaRename
 			e.replicateDoDb[doDb.TableSchema] = schemaCtx
 
 			existedTables, err := sql.ShowTables(e.db, doDb.TableSchema, e.mysqlContext.ExpandSyntaxSupport)