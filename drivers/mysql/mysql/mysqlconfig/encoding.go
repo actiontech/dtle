1449fbb17b38f17cb20c1b4416509cbfddf75212
ffffwh <ffffwh@icloud.com>
2022-03-18 16:23:32 +0800

    support gb18030, utf16, utf16le #895

--- drivers/mysql/mysql/mysqlconfig/encoding.go
+++ drivers/mysql/mysql/mysqlconfig/encoding.go
@@ -8,21 +8,19 @@ package mysqlconfig
 
 import (
 	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode/utf32"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/encoding/unicode/utf32"
 )
 
-type charsetEncoding map[string]encoding.Encoding
-
-var charsetEncodingMap charsetEncoding
-
-func init() {
-	charsetEncodingMap = make(map[string]encoding.Encoding)
-	// Begin mappings
-	charsetEncodingMap["latin1"] = charmap.Windows1252
-	charsetEncodingMap["latin2"] = charmap.Windows1250
-	charsetEncodingMap["gbk"] = simplifiedchinese.GBK
-	charsetEncodingMap["gb2312"] = simplifiedchinese.GB18030
-	charsetEncodingMap["utf32"] = utf32.UTF32(utf32.BigEndian, utf32.IgnoreBOM)
+var charsetEncodingMap = map[string]encoding.Encoding{
+	"latin1": charmap.Windows1252,
+	"latin2": charmap.Windows1250,
+	"gbk": simplifiedchinese.GBK,
+	"gb2312": simplifiedchinese.GB18030,
+	"gb18030": simplifiedchinese.GB18030,
+	"utf16": unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM),
+	"utf16le": unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM),
+	"utf32": utf32.UTF32(utf32.BigEndian, utf32.IgnoreBOM),
 }
