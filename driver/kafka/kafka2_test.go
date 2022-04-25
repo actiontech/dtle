3cca6a1bb60f81f37bf12e077850273011ce4a91
yangzhongjiao <451773851@qq.com>
2022-04-25 09:16:21 +0000

    remove unreasonable unit tests

--- driver/kafka/kafka2_test.go
+++ driver/kafka/kafka2_test.go
@@ -3,7 +3,6 @@ package kafka
 import (
 	"encoding/base64"
 	"testing"
-	"time"
 )
 
 func TestDecimalValueFromStringMysql(t *testing.T) {
@@ -20,33 +19,4 @@ func TestTimeValue(t *testing.T) {
 	}
 	test("01:02:03", 1, 2, 3, 0, false)
 	test("-800:02:03.100000", 800, 2, 3, 100000, true)
-}
-
-func TestDateTimeValue(t *testing.T) {
-	tests := []struct {
-		dateTime string
-		want     int64
-	}{
-		{
-			dateTime: "9999-12-31 23:59:59",
-			want:     253402300799000,
-		},
-		{
-			dateTime: "1900-03-02 04:06:09",
-			want:     -2203790031000,
-		},
-		{
-			dateTime: "5900-03-02 04:06:09",
-			want:     124024017969000,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.dateTime, func(t *testing.T) {
-			realDateTime := DateTimeValue(tt.dateTime, time.Local)
-			if realDateTime != tt.want {
-				t.Errorf("parse dateTime  =  %v, want %v", realDateTime, tt.want)
-			}
-		})
-	}
 }
